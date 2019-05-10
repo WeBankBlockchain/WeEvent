dde19a57685c727f9e461a8b5332383fd085d435
zjy142214 <zjy142214@163.com>
2019-05-10 18:09:51 +0800

    change HttpForwardFilter to ForwardBrokerFilter

--- weevent-governance/src/main/java/com/webank/weevent/governance/filter/ForwardBrokerFilter.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/filter/ForwardBrokerFilter.java
@@ -42,7 +42,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
-public class HttpForwardFilter implements Filter{
+public class ForwardBrokerFilter implements Filter{
 	
 	@Autowired
     BrokerService brokerService;
