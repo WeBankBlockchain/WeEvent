877f7e88349f3937b4dab07f147c68f64a4798c7
websterchen <websterchen@webank.com>
2019-05-22 21:40:20 +0800

    update jsonrpc insterface

--- weevent-broker/dist/deploy-topic-control.sh
+++ weevent-broker/dist/deploy-topic-control.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 groupId=$1
-if [[ x$groupId != x ]];then
+java_cmd="java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher"
+if [[ $# -gt 0 ]];then
     if [[ $groupId -lt 1 ]];then
         echo "groupId is error must  start at 1"
         exit -1
     fi
     echo "fisco-bcos 2.0x verison"
-    java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher ./address.txt $groupId
+    ${java_cmd} ./address.txt $groupId
 else
     echo "fisco-bcos 1.3x verison"
-    java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher ./address.txt
+    ${java_cmd} ./address.txt
 fi
