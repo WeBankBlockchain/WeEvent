71c1522c31fee04603b61bf8722c729ba446a16c
Cristic <CristicMay@gmail.com>
2019-05-23 16:16:07 +0800

    fixed java sdk client bugs and add broker stomp command properties (#19)

--- weevent-client/src/main/java/com/webank/weevent/sdk/WeEventClient.java
+++ weevent-client/src/main/java/com/webank/weevent/sdk/WeEventClient.java
@@ -8,6 +8,7 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
