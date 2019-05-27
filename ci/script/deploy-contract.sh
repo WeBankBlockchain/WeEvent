1f779645cc238e45b82771187d56f7c07d31c054
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-05-27 17:43:40 +0800

    add CI

--- ci/script/deploy-contract.sh
+++ ci/script/deploy-contract.sh
@@ -1,13 +1,2 @@
 #!/bin/bash
-groupId=$1
-if [[ x$groupId != x ]];then
-    if [[ $groupId -lt 1 ]];then
-        echo "groupId is error must  start at 1"
-        exit -1
-    fi
-    echo "fisco-bcos 2.0x verison"
-    java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher ./address.txt $groupId
-else
-    echo "fisco-bcos 1.3x verison"
-    java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher ./address.txt
-fi
+echo "it is okay!!!"
