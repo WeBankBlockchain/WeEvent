28f05acee659fcfb571013efc0ea940d155a2c7c
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-05-28 16:26:43 +0800

    add build ci bash

--- ci/script/build-ci.sh
+++ ci/script/build-ci.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-nodes="127.0.0.1:20230"
+nodes="182.254.159.91:20200"
 
 function updatefisco(){
-    cp ./weevent-broker/conf/fisco.properties ./weevent-broker/conf/fisco.properties.default
+    cp ./conf/fisco.properties ./conf/fisco.properties.default
     # set the nodes
-    sed -i "/nodes=/cnodes=${nodes}" ./weevent-broker/conf/fisco.properties
+    sed -i "/nodes=/cnodes=${nodes}" ./conf/fisco.properties
 }
 
 # deploy contract and get the address
 function getContractAddress(){
     # deploy contract
-    ./weevent-broker/deploy-contract.sh
+    ./deploy-contract.sh 1
     # get address
     contractAddress=$(cat ./address.txt)
     # set topic-controller.address
     echo "contract_address:"$contractAddress
-    sed -i "/topic-controller.address=/ctopic-controller.address=${contractAddress}" ./weevent-broker/conf/fisco.properties
+    sed -i "/topic-controller.address=/ctopic-controller.address=${contractAddress}" ./conf/fisco.properties
 }
 
 function checkService() {
     # start broker service
-    ./weevent-broker/broker.sh start
-    ./weevent-broker/check-service.sh
+    ./broker.sh start
+    ./check-service.sh
 }
 
 # clean up useless staff
 function cleanup(){
     # reset the fisco
-    rm -rf  ./weevent-broker/conf/fisco.properties
-    mv ./weevent-broker/conf/fisco.properties.default ./weevent-broker/conf/fisco.properties
+    rm -rf  ./conf/fisco.properties
+    mv ./conf/fisco.properties.default ./conf/fisco.properties
 }
 
 function main(){
