529a9c9bad30d3f41b56ec27011125aebe5e0f12
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-06-12 19:39:47 +0800

    remove useless code

--- weevent-broker/src/main/java/com/webank/weevent/broker/fisco/web3sdk/PushCallback.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fisco/web3sdk/PushCallback.java
@@ -24,13 +24,6 @@ public class PushCallback extends ChannelPushCallback {
 
         log.info("{} server:push the content {}", df.format(LocalDateTime.now()), push.getContent());
         log.info("PushCallback");
-//        ChannelResponse response = new ChannelResponse();
-//        response.setContent("receive request seq:" + String.valueOf(push.getMessageID()));
-//        response.setErrorCode(0);
-//
-//        push.sendResponse(response);
-
-        // call the notify
         String content = push.getContent();
         if (content.contains(",")) {
             // for fisco bcos 2.0.* version
