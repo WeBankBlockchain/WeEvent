baef38eb14f5ae7ef1ecbf64408c47a27d529d1c
websterchen <websterchen@webank.com>
2019-06-28 21:08:26 +0800

    update weevent.properties

--- weevent-client/src/test/java/com/webank/weevent/client/sdk/WeEventClientTest.java
+++ weevent-client/src/test/java/com/webank/weevent/client/sdk/WeEventClientTest.java
@@ -44,7 +44,7 @@ public class WeEventClientTest {
 
     @Before
     public void before() throws Exception {
-        weEventClient = IWeEventClient.build("http://127.0.0.1:8081/weevent");
+        weEventClient = IWeEventClient.build("http://127.0.0.1:8080/weevent");
         weEventClient.open(topicName);
     }
 
