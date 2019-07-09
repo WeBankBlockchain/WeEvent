126deabd2cf6e647b28ce0ac1d0a4e7952472342
wwwanhai <44886875+wwwanhai@users.noreply.github.com>
2019-07-09 16:05:02 +0800

    Modify the Adaptive effects of web pages (#88)

--- weevent-governance/web/src/login/login.vue
+++ weevent-governance/web/src/login/login.vue
@@ -6,7 +6,9 @@
     <img src="../assets/image/bg.png" class='bg_img' alt="">
     <div class='login_box'>
       <img src="../assets/image/login.png" alt="" class='login_logo'>
-      <p class='words'>区块链应用平台</p>
+      <div class='login_error'>
+        <p :class="{'show_error': show_error}">用户名或密码错误，请重新输入</p>
+      </div>
       <el-form label-position="right" :rules="rules" :model='form' ref='loginForm'>
         <el-form-item label='' prop='name'>
           <el-input v-model.trim='form.name' auto-complete="off" prefix-icon='el-icon-user' placeholder="请输入用户名"></el-input>
@@ -22,7 +24,7 @@
       </el-form>
     </div>
     <el-dialog
-      title='获取密码'
+      title='密码重置'
       :visible.sync="getPass"
       width='420px'
     >
@@ -58,6 +60,7 @@ export default{
     }
     return {
       getPass: false,
+      show_error: false,
       userName: '',
       form: {
         name: '',
@@ -93,15 +96,13 @@ export default{
             if (res.status === 200 && res.data.code === 0) {
               localStorage.setItem('userId', res.data.data.userId)
               localStorage.setItem('user', res.data.data.username)
+              this.show_error = false
               this.$router.push('./index')
             } else if (res.data.code === 202034) {
-              this.form.name = ''
               this.form.passWord = ''
-              this.$refs[formName].validate((valid))
-              this.$message({
-                type: 'warning',
-                message: '用户名或密码错误'
-              })
+              this.show_error = true
+              this.$refs[formName].fields[1].$el.style.borderColor = '#000'
+              this.$refs[formName].fields[1].$el.children[0].children[0].children[0].focus()
             }
           })
         } else {
