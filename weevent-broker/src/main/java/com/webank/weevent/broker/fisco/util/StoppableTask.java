379fc6d0d3cacc225f61c805855badfd45d3e406
saintping <saintping@gmail.com>
2019-07-15 13:20:53 +0800

    speed up spring boot initialize, use ThreadPoolTaskExecutor instead of Thread (#111)

--- weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/StoppableTask.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/StoppableTask.java
@@ -11,58 +11,35 @@ import lombok.extern.slf4j.Slf4j;
  * @since 2018/11/09
  */
 @Slf4j
-public abstract class StoppableTask extends Thread {
+public abstract class StoppableTask implements Runnable {
+    private String name;
+
     // flag to exit task
     private volatile boolean exit = false;
 
     public StoppableTask(String name) {
-        super(name);
+        this.name = name;
     }
 
     public void doExit() {
-        log.info("enter");
-
+        log.info("set exit flag, name: {}", this.name);
         this.exit = true;
-
-        for (int i = 0; i < 15; i++) {
-            try {
-                log.debug("try to graceful exit");
-                // idle 1 second
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                log.error("exit failed, due to InterruptedException");
-                Thread.currentThread().interrupt();
-            }
-
-            if (!isAlive()) {
-                log.info("not alive, doExit carefully");
-                return;
-            }
-        }
-
-        // Force terminate, Web3sdk will throw NullPointException if interrupt()
-        //this.interrupt();
-        log.info("force to exit after wait");
     }
 
     protected abstract void taskOnceLoop() throws InterruptedException;
 
     @Override
     public void run() {
-        log.info("task thread enter, name: {}", this.getName());
+        log.info("task enter, name: {}", this.name);
 
         try {
             while (!this.exit) {
-                if (isInterrupted()) {
-                    break;
-                }
                 this.taskOnceLoop();
             }
-        } catch (InterruptedException e) {
-            log.info("catch InterruptedException to exit.");
-            Thread.currentThread().interrupt();
+        } catch (Exception e) {
+            log.error("Exception in task, name: " + this.name, e);
         }
 
-        log.info("task thread exit, name: {}", this.getName());
+        log.info("task exit, name: {}", this.name);
     }
 }
