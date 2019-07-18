adb93b452b0f743bc2b37ed9a885e83450520042
websterchen <websterchen@webank.com>
2019-07-18 17:45:33 +0800

    update check jdk verison

--- weevent-broker/dist/broker.sh
+++ weevent-broker/dist/broker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#check javajdk
 source ~/.bashrc
+#check javajdk
 function check_javajdk(){
     java_version=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }' | awk -F[.] '{print $1$2}'`
     system_version=`cat /etc/os-release | awk -F'[= "]' '{print $3}' | head -1`
     if [[ ${java_version} -le 18 && ${system_version} -eq "CentOS" ]];then
-        echo "if the system is CentOS java verison must greater than 1.9"
+        echo "in CentOS, Open JDK's verison must be 1.9 or larger"
         exit -1
     fi
 }
