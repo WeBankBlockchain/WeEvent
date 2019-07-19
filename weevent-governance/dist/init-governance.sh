7c45f3633e067ea7fbac3309ce9dcd3d6bb79a0f
matthewliu <saintping@gmail.com>
2019-07-19 11:19:51 +0800

    suppress the output of source bashrc (#125)

--- weevent-governance/dist/init-governance.sh
+++ weevent-governance/dist/init-governance.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#java -Xbootclasspath/a:./conf -cp ./apps/*  -Dloader.main=com.webank.weevent.governance.initial.InitialDb org.springframework.boot.loader.PropertiesLauncher
 
 java -Xbootclasspath/a:./conf -cp ./apps/*  -Dloader.main=com.webank.weevent.governance.initial.InitialDb org.springframework.boot.loader.PropertiesLauncher
-
-if [ $? -ne 0 ];then
-    echo "init governance db failed"
-    echo "please check cofiguration of mysql"
+if [[ $? -ne 0 ]];then
+    echo "init governance db failed, please check the configuration of mysql"
     return 1
 else
    echo "init governance db success"
