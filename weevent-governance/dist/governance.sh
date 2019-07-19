ea875f404da266ec881995f2f72887470af55c77
matthewliu <saintping@gmail.com>
2019-07-19 20:42:14 +0800

    pretty bash 2 (#131)

--- weevent-governance/dist/governance.sh
+++ weevent-governance/dist/governance.sh
@@ -16,32 +16,32 @@ start(){
     get_pid;
     if [[ -n "${current_pid}" ]];then
         echo "governance is running, (PID=${current_pid})"
-    else
+        exit 0
+    fi
 
-        nohup java ${JAVA_OPTS} -Xbootclasspath/a:./conf:./html -Djava.security.egd=file:/dev/./urandom -jar ./apps/*  >/dev/null 2>&1 &
-        sleep 3
-        governance_pid=$!
-        if [[ -n "${governance_pid}" ]];then
-            echo "start governance success (PID=${governance_pid})"
-            
-            if [[ `crontab -l | grep -w governance | wc -l` -eq 0 ]]; then
-                 crontab -l > cron.backup
-                 echo "* * * * * cd `pwd`; ./governance.sh monitor >> ./logs/monitor.log 2>&1" >> cron.backup
-                 crontab cron.backup
-                 rm cron.backup
-            fi
+    nohup java ${JAVA_OPTS} -Xbootclasspath/a:./conf:./html -Djava.security.egd=file:/dev/./urandom -jar ./apps/*  >/dev/null 2>&1 &
+    sleep 3
+    governance_pid=$!
+    if [[ -n "${governance_pid}" ]];then
+        echo "start governance success (PID=${governance_pid})"
 
-            if [[ `crontab -l | grep -w governance | wc -l` -gt 0 ]]; then
-                 echo "add the crontab job success"
-                 exit 0
-            else
-                 echo "add the crontab job fail"
-                 exit 1
-            fi
-        else
-            echo "start governance fail"
-            exit 1
+        if [[ `crontab -l | grep -w governance | wc -l` -eq 0 ]]; then
+             crontab -l > cron.backup
+             echo "* * * * * cd `pwd`; ./governance.sh monitor >> ./logs/monitor.log 2>&1" >> cron.backup
+             crontab cron.backup
+             rm cron.backup
         fi
+
+        if [[ `crontab -l | grep -w governance | wc -l` -gt 0 ]]; then
+             echo "add the crontab job success"
+             exit 0
+        else
+             echo "add the crontab job fail"
+             exit 1
+        fi
+    else
+        echo "start governance fail"
+        exit 1
     fi
 }
 
