3407a8c46f4069f6053335cd2eb01dc57719ead8
matthewliu <saintping@gmail.com>
2019-07-19 11:19:51 +0800

    suppress the output of source bashrc (#125)

--- weevent-broker/dist/deploy-topic-control.sh
+++ weevent-broker/dist/deploy-topic-control.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-source ~/.bashrc
+echo "source ~/.bashrc to confirm java jdk environment"
+source ~/.bashrc >/dev/null 2>&1
 
-#check javajdk
-function check_javajdk(){
+#check java jdk, not support openjdk 1.8 in CentOS
+function check_java_jdk(){
     java_version=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }' | awk -F[.] '{print $1$2}'`
     system_version=`cat /etc/os-release | awk -F'[= "]' '{print $3}' | head -1`
-    oepnjdk=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $1 }'`
-    if [[ ${java_version} -le 18 && "${system_version}" == "CentOS" && "${openjdk}" == "oepnjdk" ]];then
-        echo "in CentOS, Open JDK's verison must be 1.9 or greater"
+    openjdk=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $1 }'`
+    if [[ ${java_version} -le 18 && "${system_version}" == "CentOS" && "${openjdk}" == "openjdk" ]];then
+        echo "in CentOS, OpenJDK's version must be 1.9 or greater"
         exit -1
     fi
 }
-check_javajdk
+check_java_jdk
 
 java_cmd="java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher"
 if [[ $# -gt 0 ]];then
     groupId=$1
-    if [[ $groupId -lt 1 ]];then
-        echo "groupId is error must  start at 1"
-        exit -1
+    if [[ ${groupId} -lt 1 ]];then
+        echo "groupId must be >= 1"
+        exit 1
     fi
-    ${java_cmd} ./address.txt $groupId
+    ${java_cmd} ./address.txt ${groupId}
 else
     ${java_cmd} ./address.txt
 fi
--- weevent-governance/dist/governance.sh
+++ weevent-governance/dist/governance.sh
@@ -1,45 +1,37 @@
 #!/bin/bash 
 
-pidfile=./logs/governance.pid
-eventgovernance_pid=
-currentgovernance_pid=
+pid_file=./logs/governance.pid
+current_pid=
 
 JAVA_OPTS="-Xverify:none -XX:TieredStopAtLevel=1 -Xms512m -Xmx2048m -XX:NewSize=256m -XX:MaxNewSize=1024m -XX:PermSize=128m -XX:+DisableExplicitGC"
 
-getTradeProtalPID(){
-    if [ -e $pidfile ]; then
-        eventgovernance_pid=`cat $pidfile`
-        currentgovernance_pid=`ps aux|grep "governance" | grep "$eventgovernance_pid" | grep -v grep|awk '{print $2}'|head -1`
+get_pid(){
+    if [[ -e ${pid_file} ]]; then
+        pid=`cat ${pid_file}`
+        current_pid=`ps aux|grep "governance" | grep "${pid}" | grep -v grep | awk '{print $2}'`
     fi
 }
 
 start(){
-    getTradeProtalPID;
-    if [ -n "$currentgovernance_pid" ];then
-        echo "governance is running, (PID=$currentgovernance_pid)"
+    get_pid;
+    if [[ -n "${current_pid}" ]];then
+        echo "governance is running, (PID=${current_pid})"
     else
 
         nohup java ${JAVA_OPTS} -Xbootclasspath/a:./conf:./html -Djava.security.egd=file:/dev/./urandom -jar ./apps/*  >/dev/null 2>&1 &
         sleep 3
-        eventgovernance_pid=$!
-        if [ -n "$eventgovernance_pid" ];then
-            echo "start governance success (PID=$eventgovernance_pid)"
-            if [ -f $pidfile ]; then
-                 echo "${eventgovernance_pid}" > $pidfile
-            else
-                 touch $pidfile;
-                 sleep 3
-                 echo "${eventgovernance_pid}" > $pidfile
-            fi
+        governance_pid=$!
+        if [[ -n "${governance_pid}" ]];then
+            echo "start governance success (PID=${governance_pid})"
             
-            if [ `crontab -l | grep -w governance | wc -l` -eq 0 ]; then
+            if [[ `crontab -l | grep -w governance | wc -l` -eq 0 ]]; then
                  crontab -l > cron.backup
                  echo "* * * * * cd `pwd`; ./governance.sh monitor >> ./logs/monitor.log 2>&1" >> cron.backup
                  crontab cron.backup
                  rm cron.backup
             fi
 
-            if [ `crontab -l | grep -w governance | wc -l` -gt 0 ]; then
+            if [[ `crontab -l | grep -w governance | wc -l` -gt 0 ]]; then
                  echo "add the crontab job success"
                  exit 0
             else
@@ -54,20 +46,20 @@ start(){
 }
 
 stop(){
-    getTradeProtalPID;
-    kill_cmd="kill -9 ${currentgovernance_pid}"
-    if [ -n "$currentgovernance_pid" ];then
+    get_pid;
+    kill_cmd="kill -9 ${current_pid}"
+    if [[ -n "${current_pid}" ]];then
         eval ${kill_cmd}
       
         echo "stop governance success"
-        if [ `crontab -l | grep -w governance | wc -l` -gt 0 ]; then
+        if [[ `crontab -l | grep -w governance | wc -l` -gt 0 ]]; then
             crontab -l>cron.backup
             sed -i '/governance/d' cron.backup
             crontab cron.backup
             rm cron.backup
         fi
 
-        if [ `crontab -l | grep -w governance | wc -l` -gt 0 ]; then
+        if [[ `crontab -l | grep -w governance | wc -l` -gt 0 ]]; then
             echo "remove the crontab job fail"
             exit 1
         else
@@ -80,23 +72,23 @@ stop(){
 }
 
 monitor(){
-    getTradeProtalPID
-    if [ -n "$currentgovernance_pid" ]; then
-        echo "`date`: governance is running(PID=$eventgovernance_pid)"
+    get_pid
+    if [[ -n "${current_pid}" ]]; then
+        echo "`date`: governance is running(PID=${current_pid})"
     else
         echo "`date`: governance is not running,restart governance now"
         start
     fi   
 }
 
-if [ $# -lt 1 ]; then
+if [[ $# -lt 1 ]]; then
     echo "Usage:"
     echo "     governance.sh start|stop"
     exit 1
 fi
 
-#do operation
-case "${1}" in
+# command list
+case "$1" in
 start)
     start
     ;;
