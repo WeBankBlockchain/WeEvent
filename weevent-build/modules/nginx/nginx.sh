5a8184fd9fe5c5efee3c3d09d4bb2d7d1e7cbce5
matthewliu <saintping@gmail.com>
2019-07-19 16:54:45 +0800

    fix SQL: exceed index max length (#127)

--- weevent-build/modules/nginx/nginx.sh
+++ weevent-build/modules/nginx/nginx.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
 current_path=`pwd`
-nginxd=$current_path/sbin/nginx
-nginx_config=$current_path/conf/nginx.conf
-nginx_pid=$current_path/logs/nginx.pid
-currentnginx_pid=
 
-[ -x $nginxd ] || { echo "not exist nginx, [$nginxd]"; exit 1; }
+nginxd=${current_path}/sbin/nginx
+nginx_config=${current_path}/conf/nginx.conf
+pid_file=${current_path}/logs/nginx.pid
+current_pid=
 
-[ -f $nginx_config ] || { echo "not exist nginx configuration, [$nginx_config]"; exit 1; }
+[[ -x ${nginxd} ]] || { echo "not exist nginx, [${nginxd}]"; exit 1; }
 
-getcurrent_pid(){
-   if [ -e $nginx_pid ]; then
-       file_pid=`cat $nginx_pid`
-       currentnginx_pid=`ps aux|grep "nginx" | grep "$file_pid" | grep -v grep|awk '{print $2}'|head -1`
+[[ -f ${nginx_config} ]] || { echo "not exist nginx configuration, [${nginx_config}]"; exit 1; }
+
+get_current_pid(){
+   if [[ -e ${pid_file} ]]; then
+       pid=`cat ${pid_file}`
+       current_pid=`ps aux | grep "nginx" | grep "${pid}" | grep -v grep | awk '{print $2}'`
    fi
 }
 
 start(){
-    getcurrent_pid;
-    if [ -n "$currentnginx_pid" ]; then
+    get_current_pid
+    if [[ -n "${current_pid}" ]]; then
         echo "nginx already running"
         exit 1
     else
-        $nginxd -c $nginx_config
+        ${nginxd} -c ${nginx_config}
     fi
    
     sleep 1
     getcurrent_pid;
-    if [ -z "$currentnginx_pid" ]; then
+    if [[ -z "${current_pid}" ]]; then
         echo "start nginx fail"
         exit 1
     else
-        echo "start nginx success (PID=$currentnginx_pid)"
-        if [ `crontab -l | grep -w nginx | wc -l` -eq 0 ]; then
+        echo "start nginx success (PID=${current_pid})"
+        if [[ `crontab -l | grep -w nginx | wc -l` -eq 0 ]]; then
             crontab -l > cron.backup
             echo "* * * * * cd `pwd`; ./nginx.sh monitor >> ./logs/monitor.log 2>&1" >> cron.backup
             crontab cron.backup
             rm cron.backup
         fi
 
-        if [ `crontab -l | grep -w nginx | wc -l` -gt 0 ]; then
+        if [[ `crontab -l | grep -w nginx | wc -l` -gt 0 ]]; then
             echo "add the crontab job success"
             exit 0
         else
@@ -51,23 +52,23 @@ start(){
 }
 
 stop(){
-    getcurrent_pid;
-    if [ -z "$currentnginx_pid" ]; then
+    get_current_pid
+    if [[ -z "${current_pid}" ]]; then
         echo "nginx is not running"
         exit 1
     else     
-        kill -QUIT `cat $nginx_pid`
+        kill -QUIT `cat ${pid_file}`
         echo "stop nginx success"
     fi
     
-    if [ `crontab -l | grep -w nginx | wc -l` -gt 0 ]; then
+    if [[ `crontab -l | grep -w nginx | wc -l` -gt 0 ]]; then
         crontab -l>cron.backup
         sed -i '/nginx/d' cron.backup
         crontab cron.backup
         rm cron.backup
     fi
 
-    if [ `crontab -l | grep -w nginx | wc -l` -gt 0 ]; then
+    if [[ `crontab -l | grep -w nginx | wc -l` -gt 0 ]]; then
         echo "remove the crontab job fail"
         exit 1
     else
@@ -76,23 +77,23 @@ stop(){
 }
 
 monitor(){
-    getcurrent_pid;
-    if [ -n "$currentnginx_pid" ]; then
-        echo "`date`: nginx is running(PID=$currentnginx_pid)"
+    getcurrent_pid
+    if [[ -n "${current_pid}" ]]; then
+        echo "`date`: nginx is running(PID=${current_pid})"
     else
-        echo "`date`: nginx is not running,restart nginx row"
+        echo "`date`: nginx is not running, restart nginx now..."
         ./nginx.sh start
     fi
 }
 
-if [ $# -lt 1 ]; then
+if [[ $# -lt 1 ]]; then
     echo "Usage:"
     echo "     nginx.sh start|stop"
     exit 1
 fi
 
-#do operation
-case "${1}" in
+# command list
+case "$1" in
 start)
     start
     ;;
