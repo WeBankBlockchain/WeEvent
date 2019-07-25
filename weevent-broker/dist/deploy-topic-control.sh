6818411bed538891f2255db98baaee8d19b2f970
WeWebsterchen <30589136+WeWebsterchen@users.noreply.github.com>
2019-07-25 11:04:20 +0800

    update  quickinstall shell (#141)

--- weevent-broker/dist/deploy-topic-control.sh
+++ weevent-broker/dist/deploy-topic-control.sh
@@ -4,12 +4,17 @@ source ~/.bashrc >/dev/null 2>&1
 
 #check java jdk, not support openjdk 1.8 in CentOS
 function check_java_jdk(){
+    java -version >>/dev/null 2>&1
+    if [[ $? -ne 0 ]];then
+        echo "not installed JDK"
+        exit 1
+    fi
     java_version=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }' | awk -F[.] '{print $1$2}'`
     system_version=`cat /etc/os-release | awk -F'[= "]' '{print $3}' | head -1`
     openjdk=`java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $1 }'`
     if [[ ${java_version} -le 18 && "${system_version}" == "CentOS" && "${openjdk}" == "openjdk" ]];then
         echo "in CentOS, OpenJDK's version must be 1.9 or greater"
-        exit -1
+        exit 1
     fi
 }
 check_java_jdk
