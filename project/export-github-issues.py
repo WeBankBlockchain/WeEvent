dab835ffdc47c40e94d62aaf32b3817d5bc4ce0c
matthewliu <saintping@gmail.com>
2019-07-30 18:51:25 +0800

    support milestone and page parameter (#153)

--- project/export-github-issues.py
+++ project/export-github-issues.py
@@ -4,62 +4,111 @@
 ###############################################################################
 
 import requests
+import sys
 import xlwt
 
-url = "https://api.github.com/repos/WeBankFinTech/WeEvent/issues"
 
-# get data from github api
-response = requests.get(url)
-origin_data = response.json()
-print("issues number: " + str(len(origin_data)))
-if not origin_data:
-    exit(1)
+def load_all_issues(milestone):
+    '''
+    load all issues in milestone
+    :param milestone: milestone in project
+    :return: True if success, list of issues
+    '''
 
-# extract need column
-columns = ["number", "html_url", "title", "body", "state", "comments", "created_at", "updated_at", "closed_at"]
-data = []
-for issue in origin_data:
-    line = {}
-    for k, v in issue.items():
-        if k in columns:
-            line[k] = v
-        elif k == "user":
-            if v and "login" in v:
-                line[k] = v["login"]
-            else:
-                line[k] = ""
-        elif k == "assignees":
-            assignee_str = ""
-            for assignee in v:
-                if assignee and "login" in assignee:
-                    assignee_str = assignee["login"] + ","
-            line[k] = assignee_str
-        elif k == "milestone":
-            if v and "title" in v:
-                line[k] = v["title"]
-            else:
-                line[k] = ""
-        elif k == "labels":
-            label_str = ""
-            for label in v:
-                if v and "name" in label:
-                    label_str = label["name"] + ","
-            line[k] = label_str
-    data.append(line)
+    # page is 1-based, 30 items by default
+    url = "https://api.github.com/repos/WeBankFinTech/WeEvent/issues?state=all&milestone=%s&per_page=100" % milestone
+    page = 1
+    origin_data = []
+    while True:
+        url_once = "%s&page=%s" % (url, page)
+        print("fetch url: %s" % url_once)
+        response = requests.get(url_once, timeout=5)
+        if response.status_code != 200:
+            print("github api status code is %s" % response.status_code)
+            return False, origin_data
+        data_once = response.json()
+        if data_once:
+            origin_data.extend(data_once)
+            page += 1
+        else:
+            break
 
-# prepare excel format, header and data list
-issues = [list(data[0].keys())]
-for issue in data:
-    issues.append(list(issue.values()))
+    print("total issues number: %s" % len(origin_data))
+    return True, origin_data
 
-# write into excel
-book = xlwt.Workbook()
-sheet = book.add_sheet("sheet1")
-row = 0
-for line in issues:
-    col = 0
-    for x in line:
-        sheet.write(row, col, str(x))
-        col += 1
-    row += 1
-book.save("./WeEvent.xls")
+
+def export_issues(milestone, output):
+    '''
+    get all issues in milestone like "v1.1.0"
+    see https://developer.github.com/v3/
+    :param milestone: milestone in project
+    :param output: excel file name
+    :return: True if success
+    '''
+
+    # get data from github api
+    result, origin_data = load_all_issues(milestone)
+    if not result:
+        return False
+
+    # extract need column
+    columns = ["number", "html_url", "title", "body", "state", "comments", "created_at", "updated_at", "closed_at"]
+    data = []
+    for issue in origin_data:
+        line = {}
+        for k, v in issue.items():
+            if k in columns:
+                line[k] = v
+            elif k == "user":
+                if v and "login" in v:
+                    line[k] = v["login"]
+                else:
+                    line[k] = ""
+            elif k == "assignees":
+                assignee_str = ""
+                for assignee in v:
+                    if assignee and "login" in assignee:
+                        assignee_str = assignee["login"] + ","
+                line[k] = assignee_str
+            elif k == "milestone":
+                if v and "title" in v:
+                    line[k] = v["title"]
+                else:
+                    line[k] = ""
+            elif k == "labels":
+                label_str = ""
+                for label in v:
+                    if v and "name" in label:
+                        label_str = label["name"] + ","
+                line[k] = label_str
+        data.append(line)
+
+    # prepare excel format, header and data list
+    issues = [list(data[0].keys())]
+    for issue in data:
+        issues.append(list(issue.values()))
+
+    # write into excel
+    book = xlwt.Workbook()
+    sheet = book.add_sheet("sheet1")
+    row = 0
+    for line in issues:
+        col = 0
+        for x in line:
+            sheet.write(row, col, str(x))
+            col += 1
+        row += 1
+
+    print("save issues into excel: %s" % output)
+    book.save(output)
+    return True
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage:\n\t%s 2\n\t%s v1.1.0" % (sys.argv[0], sys.argv[0]))
+        exit(1)
+
+    milestone = sys.argv[1]
+    print("try to load issues in milestone: %s" % milestone)
+    export_issues(milestone, "WeEvent.xls")
