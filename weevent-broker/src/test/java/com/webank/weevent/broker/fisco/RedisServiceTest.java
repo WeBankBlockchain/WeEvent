53877615a09f6ad7ce7d83801d8628f145ac9057
matthewliu <saintping@gmail.com>
2019-08-13 16:23:30 +0800

    issues#120: add WeEvent construction, rebuild junit test case (#174)

--- weevent-broker/src/test/java/com/webank/weevent/broker/fisco/RedisServiceTest.java
+++ weevent-broker/src/test/java/com/webank/weevent/broker/fisco/RedisServiceTest.java
@@ -7,54 +7,60 @@ import com.webank.weevent.JUnitTestBase;
 import com.webank.weevent.sdk.WeEvent;
 
 import lombok.extern.slf4j.Slf4j;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import static org.junit.Assert.*;
 
 @Slf4j
 public class RedisServiceTest extends JUnitTestBase {
-    
     @Autowired
     private RedisService redisService;
-    
-    private String blocknum = "1";
-    
+
+    private String blockNum = "1";
+
     @Before
     public void before() {
-	List<WeEvent> list = new ArrayList<>();
-	WeEvent event= new WeEvent();
-	event.setTopic(this.topicName);
-	event.setContent("hello".getBytes());
-	event.setEventId("317e7c4c-75-1");
-	list.add(event);
-	redisService.writeEventsToRedis(blocknum, list);
+        log.info("===================={}", this.testName.getMethodName());
+
+        List<WeEvent> list = new ArrayList<>();
+        WeEvent event = new WeEvent();
+        event.setTopic(this.topicName);
+        event.setContent("hello".getBytes());
+        event.setEventId("317e7c4c-75-1");
+        list.add(event);
+        redisService.writeEventsToRedis(blockNum, list);
     }
 
     @Test
     public void testWriteEventsToRedis() {
-	
-	String blockNum = "3";
-	List<WeEvent> list = new ArrayList<>();
-	WeEvent event= new WeEvent();
-	event.setTopic("com.weevent.test");
-	event.setContent("write to redis test".getBytes());
-	event.setEventId("sdfsff-345-3");
-	list.add(event);
-	redisService.writeEventsToRedis(blockNum, list);
+        log.info("===================={}", this.testName.getMethodName());
+
+        String blockNum = "3";
+        List<WeEvent> list = new ArrayList<>();
+        WeEvent event = new WeEvent();
+        event.setTopic("com.weevent.test");
+        event.setContent("write to redis test".getBytes());
+        event.setEventId("sdfsff-345-3");
+        list.add(event);
+        redisService.writeEventsToRedis(blockNum, list);
+        Assert.assertTrue(true);
     }
 
     @Test
     public void testReadEventsFromRedis() {
-	List<WeEvent> result = redisService.readEventsFromRedis(blocknum);
-	assertNotNull(result);
-	assertEquals(result.get(0).getTopic(), this.topicName);
+        log.info("===================={}", this.testName.getMethodName());
+
+        List<WeEvent> result = redisService.readEventsFromRedis(blockNum);
+        Assert.assertNotNull(result);
+        Assert.assertEquals(result.get(0).getTopic(), this.topicName);
     }
 
     @Test
     public void testIsEventsExistInRedis() {
-	assertTrue(redisService.isEventsExistInRedis(blocknum));
+        log.info("===================={}", this.testName.getMethodName());
+
+        Assert.assertTrue(redisService.isEventsExistInRedis(blockNum));
     }
 
 }
