d96105d03e43e914908b48457cc42f9225f15ec5
matthewliu <saintping@gmail.com>
2019-08-15 18:08:37 +0800

    support STOMP subscribe in tag (#181)

--- weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/WeEventUtils.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/WeEventUtils.java
@@ -4,28 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.webank.weevent.broker.fisco.constant.WeEventConstants;
-import com.webank.weevent.sdk.BrokerException;
-import com.webank.weevent.sdk.ErrorCode;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class WeEventUtils {
-    public static Map<String, String> getExtensions(Map<String, String> eventData) throws BrokerException {
+    public static Map<String, String> getExtensions(Map<String, String> eventData) {
         Map<String, String> extensions = new HashMap<>();
         for (Map.Entry<String, String> extension : eventData.entrySet()) {
             if (extension.getKey().startsWith(WeEventConstants.EXTENSIONS_PREFIX_CHAR)) {
                 extensions.put(extension.getKey(), extension.getValue());
-            }
-        }
-        return extensions;
-    }
-
-    public static Map<String, String> getObjectExtensions(Map<String, Object> eventData) throws BrokerException {
-        Map<String, String> extensions = new HashMap<>();
-        for (Map.Entry<String, Object> extension : eventData.entrySet()) {
-            if (extension.getKey().startsWith(WeEventConstants.EXTENSIONS_PREFIX_CHAR)) {
-                extensions.put(extension.getKey(), extension.getValue().toString());
             }
         }
         return extensions;
