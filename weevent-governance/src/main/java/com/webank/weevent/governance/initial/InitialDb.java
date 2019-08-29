924f1690398dc6e32e580ca950265e37a4f3c69c
puremilkfan <32903155+puremilkfan@users.noreply.github.com>
2019-08-29 16:37:15 +0800

    fix issue #150 : extract SQL statements from java code into separate sql (#202)

--- weevent-governance/src/main/java/com/webank/weevent/governance/initial/InitialDb.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/initial/InitialDb.java
@@ -1,15 +1,22 @@
 package com.webank.weevent.governance.initial;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.webank.weevent.governance.exception.GovernanceException;
 
 import lombok.extern.slf4j.Slf4j;
-import org.yaml.snakeyaml.Yaml;
 
 /**
  * tool to initdb
@@ -22,63 +29,73 @@ public class InitialDb {
         String user = "";
         String password = "";
         String driverName = "";
+        String dbName;
         try {
-            Yaml yaml = new Yaml();
+            Properties properties = new Properties();
             URL url = InitialDb.class.getClassLoader().getResource("application-prod.properties");
             if (url != null) {
-                Map map = (Map) yaml.load(new FileInputStream(url.getFile()));
-                Map springMap = (Map) map.get("spring");
-                Map dataSourceMap = (Map) springMap.get("datasource");
-                goalUrl = dataSourceMap.get("url").toString();
-                user = dataSourceMap.get("username").toString();
-                password = dataSourceMap.get("password").toString();
-                driverName = dataSourceMap.get("driver-class-name").toString();
+                properties.load(new FileInputStream(url.getFile()));
+                goalUrl = properties.getProperty("spring.datasource.url");
+                user = properties.getProperty("spring.datasource.username");
+                password = properties.getProperty("spring.datasource.password");
+                driverName = properties.getProperty("spring.datasource.driver-class-name");
             }
-
         } catch (Exception e) {
-            log.error(e.getMessage());
+            log.error("read database properties error,{}", e.getMessage());
         }
 
         // first use dbself database
         int first = goalUrl.lastIndexOf("/");
         int end = goalUrl.lastIndexOf("?");
-        String dbName = goalUrl.substring(first + 1, end);
+        dbName = goalUrl.substring(first + 1, end);
         // get mysql default url like jdbc:mysql://127.0.0.1:3306
         String defaultUrl = goalUrl.substring(0, first);
         Class.forName(driverName);
 
+        List<String> tableSqlList = readSql();
         try (Connection conn = DriverManager.getConnection(defaultUrl, user, password);
-                Statement stat = conn.createStatement()) {
-            // create database
-            stat.executeUpdate("create database if not exists " + dbName);
+             Statement stat = conn.createStatement()) {
+            String querySql = "SELECT count(1) FROM information_schema.SCHEMATA where SCHEMA_NAME=" + "'" + dbName + "'";
+            ResultSet resultSet = stat.executeQuery(querySql);
+            while (resultSet.next()) {
+                int num = resultSet.getInt(1);
+                if (num == 1) {
+                    log.error("database {} {}",dbName," is exist!");
+                    throw new GovernanceException("database " +dbName+" is exist!");
+                }
+            }
+            String dbSql = "create database " + dbName + " default character set utf8 collate utf8_general_ci;";
+            tableSqlList.add(0,dbSql);
+            String useDataBase = "use "+dbName+";";
+            tableSqlList.add(1,useDataBase);
+            for (String sql : tableSqlList) {
+                stat.executeUpdate(sql);
+            }
+            log.info("create database {} {}",dbName," success!");
         } catch (SQLException e) {
-            log.error(e.getMessage());
+            log.error("create database fail,message: {}",e.getMessage());
             throw e;
         }
+    }
 
-        try (Connection conn = DriverManager.getConnection(goalUrl, user, password);
-                Statement stat = conn.createStatement()) {
-            // create table t_account
-            stat.executeUpdate("create table if not exists t_account(\n"
-                    + "id int(11) not null auto_increment primary key,\n" + "email varchar(128) not null,\n"
-                    + "username varchar(128) not null unique,\n" + "password varchar(128) not null,\n"
-                    + "last_update timestamp not null,\n" + "key index_email (email),\n"
-                    + "key index_username (username)\n" + ")engine =Innodb default charset=utf8;");
-            // create table t_broker
-            stat.executeUpdate("create table if not exists t_broker(\n"
-                    + "id int(11) not null auto_increment primary key,\n" + "name varchar(128) not null,\n"
-                    + "broker_url varchar(128),\n" + "webase_url varchar(128),\n" + "user_id int(11) not null,\n"
-                    + "last_update timestamp not null,\n" + "foreign key(user_id ) references t_account(id),\n"
-                    + "key index_name (name)\n" + ")engine =Innodb default charset=utf8;");
-
-            // create table t_topic
-            stat.executeUpdate("create table if not exists t_topic(\n" + "topic_name varchar(128) not null,\n"
-                    + "creater varchar(128),\n" + "broker_id int(11) not null,\n"
-                    + "last_update timestamp NOT NULL , \n" + "foreign key(broker_id) references t_broker(id),\n"
-                    + "primary key id_topic (broker_id,topic_name)\n" + ")engine =Innodb default charset=utf8;");
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw e;
+    private static List<String> readSql() throws IOException {
+        InputStream resourceAsStream = InitialDb.class.getResourceAsStream("/script/governance.sql");//配置文件路径
+        StringBuffer sqlBuffer = new StringBuffer();
+        List<String> sqlList = new ArrayList<>();
+        byte[] buff = new byte[1024];
+        int byteRead = 0;
+        while ((byteRead = resourceAsStream.read(buff)) != -1) {
+            sqlBuffer.append(new String(buff, 0, byteRead, Charset.defaultCharset()));
         }
+        String[] sqlArr = sqlBuffer.toString().split("(;\\s*\\r\\n)|(;\\s*\\n)");
+
+        for (int i = 0; i < sqlArr.length; i++) {
+            String sql = sqlArr[i].replaceAll("--.*", "").trim();
+            if (!("").equals(sql)) {
+                sqlList.add(sql);
+            }
+            resourceAsStream.close();
+        }
+        return sqlList;
     }
 }
