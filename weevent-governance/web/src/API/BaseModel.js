ca85e02726d88ee86f12b6d63d6b1cc0c18f48fc
wwwanhai <44886875+wwwanhai@users.noreply.github.com>
2019-08-30 16:29:28 +0800

    fix issue #135: modify the resolution mechanism of the code to support more browsers (#206)

--- weevent-governance/web/src/API/BaseModel.js
+++ weevent-governance/web/src/API/BaseModel.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import {Message} from 'element-ui'
 import qs from 'qs'
 import store from '../store'
 const con = require('../../config/config.js')
