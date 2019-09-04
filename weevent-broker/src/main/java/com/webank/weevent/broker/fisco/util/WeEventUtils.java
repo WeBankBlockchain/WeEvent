fa4e25346dc0c5d48441ebd878703628e491c1de
puremilkfan <811854137@qq.com>
2019-09-04 20:17:50 +0800

    Remove the extra checksum and modify the comment

--- weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/WeEventUtils.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/WeEventUtils.java
@@ -1,8 +1,6 @@
 package com.webank.weevent.broker.fisco.util;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -14,25 +12,21 @@ import lombok.extern.slf4j.Slf4j;
 public class WeEventUtils {
     public static Map<String, String> getExtensions(Map<String, String> eventData) {
         Map<String, String> extensions = new HashMap<>();
-        for (Map.Entry<String, String> extension : eventData.entrySet()) {
+        for (Map.Entry<String, String>extension : eventData.entrySet()) {
             if (extension.getKey().startsWith(WeEventConstants.EXTENSIONS_PREFIX_CHAR)) {
-                extensions.put(extension.getKey(), getValue(extension));
+                extensions.put(extension.getKey(), extension.getValue());
             }
         }
         return extensions;
     }
 
-    private static String getValue(Map.Entry<String, String> extension) {
-        if (extension.getValue() == null) {
-            return null;
+    public static Map<String, String> getExten(Map<String, List<String>> eventData) {
+        Map<String, String> extensions = new HashMap<>();
+        for (Map.Entry<String, List<String>>extension : eventData.entrySet()) {
+            if (extension.getKey().startsWith(WeEventConstants.EXTENSIONS_PREFIX_CHAR)) {
+                extensions.put(extension.getKey(), extension.getValue().get(0));
+            }
         }
-        List<Object> list = Arrays.asList(extension.getValue());
-        Object object = list.get(0);
-        LinkedList linkedList = null;
-        if (object instanceof LinkedList) {
-            linkedList = (LinkedList) object;
-        }
-        String returnValue = linkedList == null ? extension.getValue() : linkedList.get(0).toString();
-        return returnValue;
+        return extensions;
     }
 }
