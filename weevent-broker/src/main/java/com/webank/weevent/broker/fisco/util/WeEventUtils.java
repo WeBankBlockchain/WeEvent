496da78ed1aaadbf1e1a11aca28d6861a821a835
puremilkfan <811854137@qq.com>
2019-09-05 22:29:15 +0800

    optimized code

--- weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/WeEventUtils.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/WeEventUtils.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class WeEventUtils {
     public static Map<String, String> getExtensions(Map<String, String> eventData) {
         Map<String, String> extensions = new HashMap<>();
-        for (Map.Entry<String, String>extension : eventData.entrySet()) {
+        for (Map.Entry<String, String> extension : eventData.entrySet()) {
             if (extension.getKey().startsWith(WeEventConstants.EXTENSIONS_PREFIX_CHAR)) {
                 extensions.put(extension.getKey(), extension.getValue());
             }
@@ -20,9 +20,9 @@ public class WeEventUtils {
         return extensions;
     }
 
-    public static Map<String, String> getExten(Map<String, List<String>> eventData) {
+    public static Map<String, String> getExtension(Map<String, List<String>> eventData) {
         Map<String, String> extensions = new HashMap<>();
-        for (Map.Entry<String, List<String>>extension : eventData.entrySet()) {
+        for (Map.Entry<String, List<String>> extension : eventData.entrySet()) {
             if (extension.getKey().startsWith(WeEventConstants.EXTENSIONS_PREFIX_CHAR)) {
                 extensions.put(extension.getKey(), extension.getValue().get(0));
             }
