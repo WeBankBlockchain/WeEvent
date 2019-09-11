3d3a8247171f60449685e07fe4e13548a8f54a5d
puremilkfan <811854137@qq.com>
2019-09-11 15:25:58 +0800

    add subscribe class

--- weevent-jmeter/src/main/java/com/webank/weevent/jmeter/consumer/WeEventConsumer.java
+++ weevent-jmeter/src/main/java/com/webank/weevent/jmeter/consumer/WeEventConsumer.java
@@ -1,20 +1,8 @@
+package com.webank.weevent.jmeter.consumer;
 
-package com.webank.weevent.jmeter.producer;
-
-
-/**
- * WeEvent publish performance.
- * puremilkfan
- *
- * @since 2019/09/11
- */
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import com.webank.weevent.sdk.BrokerException;
 import com.webank.weevent.sdk.IWeEventClient;
-import com.webank.weevent.sdk.SendResult;
 import com.webank.weevent.sdk.WeEvent;
 
 import org.apache.jmeter.config.Arguments;
@@ -27,31 +15,25 @@ import org.slf4j.Logger;
  * WeEvent consumer performance.
  * puremilkfan
  *
- * @since 2019/09/10
+ * @since 2019/09/11
  */
 
-
-public class WeEventProducer extends AbstractJavaSamplerClient {
+public class WeEventConsumer extends AbstractJavaSamplerClient {
     private String topic = "com.webank.weevent.jmeter";
-    private String content = "this is jmeter test!";
-    private String format = "json";
-
-    private Map<String, String> extensions = new ConcurrentHashMap<>();
 
     private String groupId = WeEvent.DEFAULT_GROUP_ID;
 
     private IWeEventClient weEventClient;
 
-    private WeEvent weEvent;
-
     private final String http = "http://";
 
-    private static String defaultUrl = "127.0.0.1:7000";
+    private String defaultUrl = "127.0.0.1:7000";
 
     // 每个压测线程启动时跑一次
     @Override
     public void setupTest(JavaSamplerContext context) {
-        getNewLogger().info("this is producer setupTest");
+        super.setupTest(context);
+        getNewLogger().info("this is consumer setupTest");
         super.setupTest(context);
         try {
             this.defaultUrl = context.getParameter("url") == null ? this.defaultUrl : context.getParameter("url");
@@ -59,13 +41,7 @@ public class WeEventProducer extends AbstractJavaSamplerClient {
             getNewLogger().info("weEventClient:{}", this.weEventClient);
 
             this.topic = context.getParameter("topic") == null ? this.topic : context.getParameter("topic");
-            this.content = context.getParameter("content") == null ? this.content : context.getParameter("content");
-            this.format = context.getParameter("format") == null ? this.format : context.getParameter("format");
             this.groupId = context.getParameter("groupId") == null ? WeEvent.DEFAULT_GROUP_ID : context.getParameter("groupId");
-            extensions.put(WeEvent.WeEvent_FORMAT, format);
-            weEvent = new WeEvent(this.topic, this.content.getBytes(), this.extensions);
-            getNewLogger().info("weEvent:{}", weEvent);
-
             boolean result = this.weEventClient.open(this.topic);
             getNewLogger().info("open topic result: {}", result);
         } catch (BrokerException e) {
@@ -76,17 +52,15 @@ public class WeEventProducer extends AbstractJavaSamplerClient {
     // 每次runTest运行完执行
     @Override
     public void teardownTest(JavaSamplerContext context) {
-        getNewLogger().debug(getClass().getName() + ": teardownTest");
-
+        super.teardownTest(context);
     }
 
-    // JMeter GUI params
+    // Jmeter GUI参数
     @Override
     public Arguments getDefaultParameters() {
         Arguments arguments = new Arguments();
         arguments.addArgument("topic", this.topic);
-        arguments.addArgument("content", this.content);
-        arguments.addArgument("format", this.format);
+        arguments.addArgument("groupId", this.groupId);
         arguments.addArgument("url", this.defaultUrl);
         return arguments;
     }
@@ -95,17 +69,25 @@ public class WeEventProducer extends AbstractJavaSamplerClient {
     @Override
     public SampleResult runTest(JavaSamplerContext context) {
         SampleResult result = new SampleResult();
-        result.setSampleLabel("producer");
+        result.setSampleLabel("consumer");
         try {
-
             result.sampleStart();
-            SendResult sendResult = this.weEventClient.publish(this.weEvent, this.groupId);
-            result.sampleEnd();
-            result.setSuccessful(sendResult.getStatus() == SendResult.SendResultStatus.SUCCESS && sendResult.getEventId().length() > 0);
-            result.setResponseMessage(sendResult.getEventId());
+            String subscribeId = this.weEventClient.subscribe(this.topic, this.groupId, WeEvent.OFFSET_LAST, new IWeEventClient.EventListener() {
+                @Override
+                public void onEvent(WeEvent event) {
+                    getNewLogger().info("event,{}", event);
+                }
 
+                @Override
+                public void onException(Throwable e) {
+                    getNewLogger().error("subscribe Exception", e);
+                }
+            });
+            result.setSuccessful(true);
+            result.setResponseMessage(subscribeId);
+            result.sampleEnd();
         } catch (Exception e) {
-            getNewLogger().error("publish exception", e);
+            getNewLogger().error("subscribe exception", e);
             result.sampleEnd();
             result.setSuccessful(false);
             result.setResponseMessage(e.getMessage());
@@ -113,17 +95,17 @@ public class WeEventProducer extends AbstractJavaSamplerClient {
         return result;
     }
 
-    public WeEventProducer() {
-        super();
-    }
-
-
     @Override
     protected Logger getNewLogger() {
         return super.getNewLogger();
     }
 
+    public static void main(String[] args) {
 
+    }
+
+    public WeEventConsumer() {
+        super();
+    }
 }
-
 
