dbda745bfb576a307ada020e7316511e42f794c3
puremilkfan <811854137@qq.com>
2019-09-16 11:51:11 +0800

    Optimize the government code structure

--- weevent-governance/src/main/java/com/webank/weevent/governance/filter/ForwardWebaseFilter.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/filter/ForwardWebaseFilter.java
@@ -1,17 +1,10 @@
 package com.webank.weevent.governance.filter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -19,30 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.webank.weevent.governance.entity.BrokerEntity;
 import com.webank.weevent.governance.service.BrokerService;
-import com.webank.weevent.governance.utils.SpringContextUtil;
+import com.webank.weevent.governance.service.CommonService;
 
-import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class ForwardWebaseFilter implements Filter {
 
     @Autowired
-    BrokerService brokerService;
+    private BrokerService brokerService;
+
+    @Autowired
+    private CommonService commonService;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -59,141 +44,14 @@ public class ForwardWebaseFilter implements Filter {
         String webaseUrl = brokerEntity.getWebaseUrl();
         // get complete url of webase
         String newUrl = webaseUrl + subStrUrl;
-        // get client according url
-        CloseableHttpClient client = generateHttpClient(newUrl);
-        CloseableHttpResponse closeResponse = null;
-        if (req.getMethod().equals("GET")) {
-            HttpGet get = getMethod(newUrl, req);
-            closeResponse = client.execute(get);
-        } else {
-            HttpPost postMethod = postMethod(newUrl, req);
-            closeResponse = client.execute(postMethod);
-        }
 
-        String mes = EntityUtils.toString(closeResponse.getEntity());
-        log.info("response: " + mes);
-        Header encode = closeResponse.getFirstHeader("Content-Type");
-        res.setHeader(encode.getName(), encode.getValue());
-        ServletOutputStream out = response.getOutputStream();
-        out.write(mes.getBytes());
+        // get client according url
+        CloseableHttpResponse closeResponse = commonService.getCloseResponse(req, newUrl);
+        commonService.writeResponse(closeResponse,res);
         return;
         // chain.doFilter(request, response);
     }
 
-    /**
-     * return HttpGet
-     *
-     */
-    private HttpGet getMethod(String uri, HttpServletRequest request) {
-        try {
-            URIBuilder builder = new URIBuilder(uri);
-            Enumeration<String> enumeration = request.getParameterNames();
-            while (enumeration.hasMoreElements()) {
-                String nex = enumeration.nextElement();
-                builder.setParameter(nex, request.getParameter(nex));
-            }
-            return new HttpGet(builder.build());
-        } catch (URISyntaxException e) {
-            log.error(e.getMessage());
-        }
-        return null;
-    }
 
-    private HttpPost postMethod(String uri, HttpServletRequest request) {
-        StringEntity entity = null;
-        if (request.getContentType().contains("json")) {
-            entity = jsonData(request);
-        } else {
-            entity = formData(request);
-        }
-        HttpPost httpPost = new HttpPost(uri);
-        httpPost.setHeader("Content-Type", request.getHeader("Content-Type"));
-        httpPost.setEntity(entity);
-        return httpPost;
-    }
-
-    // generate CloseableHttpClient from url
-    private CloseableHttpClient generateHttpClient(String url) {
-        if (url.startsWith("https")) {
-            CloseableHttpClient bean = (CloseableHttpClient) SpringContextUtil.getBean("httpsClient");
-            return bean;
-        } else {
-            CloseableHttpClient bean = (CloseableHttpClient) SpringContextUtil.getBean("httpClient");
-            return bean;
-        }
-    }
-
-    public UrlEncodedFormEntity formData(HttpServletRequest request) {
-        UrlEncodedFormEntity urlEncodedFormEntity = null;
-        try {
-            List<NameValuePair> pairs = new ArrayList<>();
-            Enumeration<String> params = request.getParameterNames();
-            while (params.hasMoreElements()) {
-                String name = params.nextElement();
-                pairs.add(new BasicNameValuePair(name, request.getParameter(name)));
-            }
-
-            urlEncodedFormEntity = new UrlEncodedFormEntity(pairs, request.getCharacterEncoding());
-        } catch (UnsupportedEncodingException e) {
-            log.error(e.getMessage());
-        }
-        return urlEncodedFormEntity;
-    }
-
-    public StringEntity jsonData(HttpServletRequest request) {
-        InputStreamReader is = null;
-        BufferedReader reader = null;
-        try {
-            is = new InputStreamReader(request.getInputStream(), request.getCharacterEncoding());
-            reader = new BufferedReader(is);
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
-            }
-            return new StringEntity(sb.toString(), request.getCharacterEncoding());
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                log.error(e.getMessage());
-            }
-            try {
-                if (is != null) {
-                    is.close();
-                }
-            } catch (IOException e) {
-                log.error(e.getMessage());
-            }
-        }
-        return null;
-    }
-
-    public void fileHandle(HttpServletResponse response, CloseableHttpResponse closeableHttpResponse) {
-        ServletOutputStream out = null;
-        try {
-            Header encode = closeableHttpResponse.getFirstHeader("Content-Type");
-            response.setHeader(encode.getName(), encode.getValue());
-            HttpEntity entity = closeableHttpResponse.getEntity();
-
-            out = response.getOutputStream();
-            entity.writeTo(out);
-            out.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-            } catch (IOException e) {
-                log.error(e.getMessage());
-            }
-        }
-    }
 
 }
---
+++ weevent-governance/src/main/java/com/webank/weevent/governance/service/CommonService.java
@@ -0,0 +1,190 @@
+package com.webank.weevent.governance.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.webank.weevent.governance.code.ErrorCode;
+import com.webank.weevent.governance.exception.GovernanceException;
+import com.webank.weevent.governance.utils.SpringContextUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.Header;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CommonService {
+
+    private static final String HTTPS = "https";
+    private static final String HTTPS_CLIENT = "httpsClient";
+
+    private static final String HTTP_CLIENT = "httpClient";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String METHOD_TYPE = "GET";
+    private static final String FORMAT_TYPE = "json";
+
+
+    public CloseableHttpResponse getCloseResponse(HttpServletRequest req, String newUrl) throws ServletException {
+        CloseableHttpResponse closeResponse;
+        try {
+            CloseableHttpClient client = this.generateHttpClient(newUrl);
+            if (req.getMethod().equals(METHOD_TYPE)) {
+                HttpGet get = this.getMethod(newUrl, req);
+                closeResponse = client.execute(get);
+            } else {
+                HttpPost postMethod = this.postMethod(newUrl, req);
+                closeResponse = client.execute(postMethod);
+            }
+        } catch (Exception e) {
+            log.error("getCloseResponse fail,error:{}", e.getMessage());
+            throw new ServletException(e.getMessage());
+        }
+        return closeResponse;
+    }
+
+    public HttpGet getMethod(String uri, HttpServletRequest request) throws GovernanceException {
+        try {
+            URIBuilder builder = new URIBuilder(uri);
+            Enumeration<String> enumeration = request.getParameterNames();
+            while (enumeration.hasMoreElements()) {
+                String nex = enumeration.nextElement();
+                builder.setParameter(nex, request.getParameter(nex));
+            }
+            return new HttpGet(builder.build());
+        } catch (URISyntaxException e) {
+            log.error("build url method fail,error:{}", e.getMessage());
+            throw new GovernanceException(ErrorCode.BUILD_URL_METHOD);
+        }
+    }
+
+    private HttpPost postMethod(String uri, HttpServletRequest request) {
+        StringEntity entity;
+        if (request.getContentType().contains(FORMAT_TYPE)) {
+            entity = jsonData(request);
+        } else {
+            entity = ResourceReadUtil.jsonData(request);
+        }
+        HttpPost httpPost = new HttpPost(uri);
+        httpPost.setHeader(CONTENT_TYPE, request.getHeader(CONTENT_TYPE));
+        httpPost.setEntity(entity);
+        return httpPost;
+    }
+
+    private StringEntity jsonData(HttpServletRequest request) {
+        InputStreamReader is = null;
+        BufferedReader reader = null;
+        try {
+            is = new InputStreamReader(request.getInputStream(), request.getCharacterEncoding());
+            reader = new BufferedReader(is);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            return new StringEntity(sb.toString(), request.getCharacterEncoding());
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+        }
+        return null;
+    }
+
+
+    private UrlEncodedFormEntity formData(HttpServletRequest request) {
+        UrlEncodedFormEntity urlEncodedFormEntity = null;
+        try {
+            List<NameValuePair> pairs = new ArrayList<>();
+            Enumeration<String> params = request.getParameterNames();
+            while (params.hasMoreElements()) {
+                String name = params.nextElement();
+                pairs.add(new BasicNameValuePair(name, request.getParameter(name)));
+            }
+
+            urlEncodedFormEntity = new UrlEncodedFormEntity(pairs, request.getCharacterEncoding());
+        } catch (UnsupportedEncodingException e) {
+            log.error(e.getMessage());
+        }
+        return urlEncodedFormEntity;
+    }
+
+    /**
+     * return HttpGet
+     */
+
+    // generate CloseableHttpClient from url
+    public CloseableHttpClient generateHttpClient(String url) {
+        CloseableHttpClient bean;
+        if (url.startsWith(HTTPS)) {
+            bean = (CloseableHttpClient) SpringContextUtil.getBean(HTTPS_CLIENT);
+        } else {
+            bean = (CloseableHttpClient) SpringContextUtil.getBean(HTTP_CLIENT);
+        }
+        return bean;
+    }
+
+    public void writeResponse(CloseableHttpResponse closeResponse, HttpServletResponse res) throws IOException {
+        String mes = EntityUtils.toString(closeResponse.getEntity());
+        log.info("response: " + mes);
+        Header encode = closeResponse.getFirstHeader(CONTENT_TYPE);
+        res.setHeader(encode.getName(), encode.getValue());
+        ServletOutputStream out = res.getOutputStream();
+        out.write(mes.getBytes());
+    }
+
+    static class ResourceReadUtil implements AutoCloseable {
+        public static StringEntity jsonData(HttpServletRequest request) {
+            try (InputStreamReader is = new InputStreamReader(request.getInputStream(), request.getCharacterEncoding());
+                 BufferedReader reader = new BufferedReader(is)) {
+                StringBuilder sb = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                return new StringEntity(sb.toString(), request.getCharacterEncoding());
+            } catch (IOException e) {
+                log.error(e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        public void close() throws Exception {
+            log.info("resource is close");
+        }
+    }
+}
