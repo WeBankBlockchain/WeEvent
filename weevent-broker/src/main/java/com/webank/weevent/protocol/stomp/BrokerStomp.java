51ac2b5ec76233ee47aa9a42b94c03962c2e398f
anyspa <35908094+anyspa@users.noreply.github.com>
2019-09-18 11:42:26 +0800

    issue #149: java sdk support multiple topic subscribe (#225)

--- weevent-broker/src/main/java/com/webank/weevent/protocol/stomp/BrokerStomp.java
+++ weevent-broker/src/main/java/com/webank/weevent/protocol/stomp/BrokerStomp.java
@@ -3,6 +3,7 @@ package com.webank.weevent.protocol.stomp;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -458,10 +459,9 @@ public class BrokerStomp extends TextWebSocketHandler {
         log.info("destination: {} header subscribe id: {} group id: {}", simpDestination, headerIdStr, groupId);
 
         String[] curTopicList;
-        if (simpDestination.contains(",")) {
-            // NOT support
+        if (simpDestination.contains(WeEvent.MULTIPLE_TOPIC_SEPARATOR)) {
             log.info("subscribe topic list");
-            curTopicList = simpDestination.split(",");
+            curTopicList = simpDestination.split(WeEvent.MULTIPLE_TOPIC_SEPARATOR);
         } else {
             curTopicList = new String[]{simpDestination};
         }
@@ -484,8 +484,8 @@ public class BrokerStomp extends TextWebSocketHandler {
             ext.put(IConsumer.SubscribeExt.TopicTag, tag);
         }
 
-        // support only one topic
-        String subscriptionId = this.iconsumer.subscribe(curTopicList[0],
+        // support both single/multiple topic
+        String subscriptionId = this.iconsumer.subscribe(curTopicList,
                 groupId,
                 subEventId,
                 ext,
@@ -507,8 +507,9 @@ public class BrokerStomp extends TextWebSocketHandler {
                 });
 
         log.info("bind context, session id: {} header subscription id: {} consumer subscription id: {} topic: {}",
-                session.getId(), headerIdStr, subscriptionId, curTopicList[0]);
-        sessionContext.get(session.getId()).put(headerIdStr, new Pair<>(subscriptionId, curTopicList[0]));
+                session.getId(), headerIdStr, subscriptionId, Arrays.toString(curTopicList));
+        sessionContext.get(session.getId())
+                .put(headerIdStr, new Pair<>(subscriptionId, StringUtils.join(curTopicList,WeEvent.MULTIPLE_TOPIC_SEPARATOR)));
 
         log.info("consumer subscribe success, consumer subscriptionId: {}", subscriptionId);
         return subscriptionId;
