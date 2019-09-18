e68d476d1bd9f1118b31f510d51568a03520c02b
matthewliu <saintping@gmail.com>
2019-09-18 19:13:04 +0800

    issue #101: thin spring boot jar (#229)

--- weevent-governance/dist/init-governance.sh
+++ weevent-governance/dist/init-governance.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+
 JAVA_HOME=
 
-
-if [ -z ${JAVA_HOME} ];then
+if [[ -z ${JAVA_HOME} ]];then
    echo "JAVA_HOME is null, please set it first"
    exit 1
 fi
 
-${JAVA_HOME}/bin/java -Xbootclasspath/a:./conf -cp ./apps/*  -Dloader.main=com.webank.weevent.governance.initial.InitialDb org.springframework.boot.loader.PropertiesLauncher
+${JAVA_HOME}/bin/java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.path=./lib -Dloader.main=com.webank.weevent.governance.initial.InitialDb org.springframework.boot.loader.PropertiesLauncher
 if [[ $? -ne 0 ]];then
     echo "init governance db failed, please check the configuration of mysql"
     exit 1
