485a44c4ddbcd84b2cd9c3ba25740600100a1344
matthewliu <saintping@gmail.com>
2019-09-30 15:44:45 +0800

    fix codacy issue in mqtt (#238)

---
+++ weevent-broker/src/main/java/com/webank/weevent/protocol/mqtt/command/PubAck.java
@@ -0,0 +1,28 @@
+package com.webank.weevent.protocol.mqtt.command;
+
+import com.webank.weevent.protocol.mqtt.store.IMessageIdStore;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.mqtt.MqttMessageIdVariableHeader;
+import io.netty.util.AttributeKey;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author websterchen
+ * @version v1.0
+ * @since 2019/6/5
+ */
+@Slf4j
+public class PubAck {
+    private IMessageIdStore iMessageIdStore;
+
+    public PubAck(IMessageIdStore iMessageIdStore) {
+        this.iMessageIdStore = iMessageIdStore;
+    }
+
+    public void processPubAck(Channel channel, MqttMessageIdVariableHeader variableHeader) {
+        int messageId = variableHeader.messageId();
+        log.debug("PUBACK - clientId: {}, messageId: {}", (String) channel.attr(AttributeKey.valueOf("clientId")).get(), messageId);
+        iMessageIdStore.releaseMessageId(messageId);
+    }
+}
