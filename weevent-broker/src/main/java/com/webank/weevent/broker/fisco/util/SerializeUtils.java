36044c5063eeb3b7894d3678d5dd93b339bc9f13
matthewliu <saintping@gmail.com>
2019-09-30 15:44:45 +0800

    fix codacy issue in mqtt (#238)

--- weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/SerializeUtils.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fisco/util/SerializeUtils.java
@@ -17,59 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 @SuppressWarnings(value = "unchecked")
 public class SerializeUtils {
     /**
-     * serialize object to bytes
-     *
-     * @param object object
-     * @return buf
-     */
-    public static <T> byte[] serialize(T object) {
-        if (object == null) {
-            throw new NullPointerException("Can't serialize null");
-        }
-
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-             ObjectOutputStream os = new ObjectOutputStream(bos)) {
-            os.writeObject(object);
-            return bos.toByteArray();
-        } catch (IOException e) {
-            log.error("Exception happened while serialize", e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * deserialize bytes to object
-     *
-     * @param in buf
-     * @return object
-     */
-    public static <T> T deserialize(byte[] in) {
-        if (in != null) {
-            try (ByteArrayInputStream bis = new ByteArrayInputStream(in);
-                 ObjectInputStream is = new ObjectInputStream(bis)) {
-
-
-                return (T) is.readObject();
-            } catch (IOException | ClassNotFoundException e) {
-                log.error("Exception happened while deserialize", e);
-                throw new RuntimeException(e);
-            }
-        }
-
-        return null;
-    }
-
-    /**
      * serialize object list to bytes
      *
      * @param objectList object list
      * @return buf
      */
     public static <T> byte[] serializeList(List<T> objectList) {
-        if (objectList == null) {
-            throw new NullPointerException("Can't serialize null");
-        }
-
         try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
              ObjectOutputStream os = new ObjectOutputStream(bos)) {
             for (T object : objectList) {
@@ -79,7 +32,7 @@ public class SerializeUtils {
             return bos.toByteArray();
         } catch (IOException e) {
             log.error("Exception happened while serialize", e);
-            throw new RuntimeException(e);
+            return null;
         }
     }
 
@@ -91,7 +44,6 @@ public class SerializeUtils {
      */
     public static <T> List<T> deserializeList(byte[] in) {
         List<T> objectList = new ArrayList<>();
-
         if (in != null) {
             try (ByteArrayInputStream bis = new ByteArrayInputStream(in);
                  ObjectInputStream is = new ObjectInputStream(bis)) {
@@ -105,7 +57,7 @@ public class SerializeUtils {
                 }
             } catch (IOException | ClassNotFoundException e) {
                 log.error("Exception happened while deserialize", e);
-                throw new RuntimeException(e);
+                return objectList;
             }
         }
 
