2a9f91f801ec6e172716342ddfb8bad340d3eb5b
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-10-14 15:28:50 +0800

    remove useless code

--- weevent-processor/src/main/java/com/webank/weevent/processor/jobs/SimpleJobImpl.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/jobs/SimpleJobImpl.java
@@ -1,4 +1,11 @@
-package com.webank.weevent.processor.jobs;
-
-public class SimpleJobImpl {
-}
+//package com.webank.weevent.processor.jobs;
+//
+//import com.dangdang.ddframe.job.api.ShardingContext;
+//import com.dangdang.ddframe.job.api.simple.SimpleJob;
+//
+//public class SimpleJobImpl implements SimpleJob {
+//    @Override
+//    public void execute(ShardingContext shardingContext) {
+//
+//    }
+//}
