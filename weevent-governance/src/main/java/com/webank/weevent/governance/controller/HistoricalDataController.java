e8d25cc48ba923f5d4c1cc57cee571c02a4c6863
puremilkfan <32903155+puremilkfan@users.noreply.github.com>
2019-10-18 11:42:51 +0800

    the issue #160：Governance adds historical statistics for topic dimensions (#245)

---
+++ weevent-governance/src/main/java/com/webank/weevent/governance/controller/HistoricalDataController.java
@@ -0,0 +1,40 @@
+package com.webank.weevent.governance.controller;
+
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.webank.weevent.governance.entity.HistoricalDataEntity;
+import com.webank.weevent.governance.exception.GovernanceException;
+import com.webank.weevent.governance.result.GovernanceResult;
+import com.webank.weevent.governance.service.HistoricalDataService;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "/historicalData")
+@Slf4j
+public class HistoricalDataController {
+
+    @Autowired
+    private HistoricalDataService historicalDataService;
+
+    @PostMapping("/list")
+    public GovernanceResult historicalDataList(@RequestBody HistoricalDataEntity historicalDataEntity, HttpServletRequest request,
+                                               HttpServletResponse response) throws GovernanceException {
+        log.info("get  historicalDataEntity:{} ", historicalDataEntity);
+        Map<String, List<Integer>> returnMap = historicalDataService.historicalDataList(historicalDataEntity, request, response);
+        return new GovernanceResult(returnMap);
+    }
+
+}
