754322e475c011a9ae0f8722c24053454429f5ae
shubiaolin <shubiaoyu@126.com>
2019-10-23 19:03:59 +0800

    fixbug: when publish event by stomp, message.payload is the event

--- weevent-broker/src/main/java/com/webank/weevent/protocol/stomp/BrokerStomp.java
+++ weevent-broker/src/main/java/com/webank/weevent/protocol/stomp/BrokerStomp.java
@@ -200,7 +200,7 @@ public class BrokerStomp extends TextWebSocketHandler {
 
         try {
             String simpDestination = getSimpDestination(msg);
-            SendResult sendResult = handleSend(msg, simpDestination, extensions, groupId);
+            SendResult sendResult = handleSend(msg, groupId);
 
             // package the return frame
             StompCommand command = StompCommand.RECEIPT;
@@ -421,17 +421,26 @@ public class BrokerStomp extends TextWebSocketHandler {
 
     /**
      * @param msg message
-     * @param simpDestination topic name
      */
     private SendResult handleSend(Message<byte[]> msg,
-                                  String simpDestination,
-                                  Map<String, String> extensions,
                                   String groupId) throws BrokerException {
         if (!this.iproducer.startProducer()) {
             log.error("producer start failed");
         }
 
-        SendResult sendResult = this.iproducer.publish(new WeEvent(simpDestination, msg.getPayload(), extensions), groupId);
+        ObjectMapper mapper = new ObjectMapper();
+        WeEvent event = null;
+        try {
+            event = mapper.readValue(msg.getPayload(), WeEvent.class);
+        } catch (IOException e) {
+            log.error("read payload from msg failed, e ", e);
+            throw new BrokerException("read payload from msg failed");
+        }
+        if (event == null){
+            log.error("payload in msg is null");
+            throw new BrokerException("payload in msg is null");
+        }
+        SendResult sendResult = this.iproducer.publish(event, groupId);
         log.info("publish result, {}", sendResult);
         if (sendResult.getStatus() != SendResult.SendResultStatus.SUCCESS) {
             log.error("producer publish failed");
