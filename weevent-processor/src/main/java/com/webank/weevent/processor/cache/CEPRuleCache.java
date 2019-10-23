c38247333ee6e42234224c77bb23b7b522848703
Cristic <CristicMay@gmail.com>
2019-10-23 17:54:19 +0800

    fix issue #104 , add weevent processor module (#247)

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/cache/CEPRuleCache.java
@@ -0,0 +1,22 @@
+package com.webank.weevent.processor.cache;
+
+import com.webank.weevent.processor.model.CEPRule;
+import com.webank.weevent.processor.mq.CEPRuleMQ;
+import com.webank.weevent.sdk.BrokerException;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+import java.util.*;
+
+
+@Slf4j
+public class CEPRuleCache {
+
+
+    public static void updateCEPRule(CEPRule rule, Map<String, CEPRule> ruleMap) throws BrokerException {
+        CEPRuleMQ.updateSubscribeMsg(rule, ruleMap);
+    }
+
+}
+
---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/mq/CEPRuleMQ.java
@@ -0,0 +1,201 @@
+package com.webank.weevent.processor.mq;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.webank.weevent.processor.model.CEPRule;
+import com.webank.weevent.processor.service.AnalysisWeEventIdService;
+import com.webank.weevent.processor.utils.CommonUtil;
+import com.webank.weevent.sdk.BrokerException;
+import com.webank.weevent.sdk.IWeEventClient;
+import com.webank.weevent.sdk.WeEvent;
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.jexl3.JexlBuilder;
+import org.apache.commons.jexl3.JexlContext;
+import org.apache.commons.jexl3.JexlEngine;
+import org.apache.commons.jexl3.MapContext;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+public class CEPRuleMQ {
+    // <ruleId <--> subscriptionId>
+    public static Map<String, String> subscriptionIdMap = new ConcurrentHashMap<>();
+
+
+    public static void updateSubscribeMsg(CEPRule rule, Map<String, CEPRule> ruleMap) throws BrokerException {
+        // unsubscribe old the topic
+        ruleMap.get(rule.getId()).getToDestination();
+        IWeEventClient client = getClient(rule);
+        // update the rule map
+        ruleMap.put(rule.getId(), rule);
+        // update subscribe
+        subscribeMsg(rule, ruleMap);
+        client.unSubscribe(subscriptionIdMap.get(rule.getId()));
+    }
+
+    private static IWeEventClient getClient(CEPRule rule) {
+        try {
+            Map<String, String> mapRequest = CommonUtil.uRLRequest(rule.getBrokerUrl());
+            String baseUrl = CommonUtil.urlPage(rule.getBrokerUrl());
+            IWeEventClient client;
+            if (null != mapRequest.get("groupId")) {
+                //  client = IWeEventClient.build(baseUrl,mapRequest.get("groupId"));
+                client = IWeEventClient.build(baseUrl);
+            } else {
+                client = IWeEventClient.build(baseUrl);
+            }
+            return client;
+        } catch (BrokerException e) {
+            log.info("BrokerException{}", e.toString());
+            return null;
+        }
+
+    }
+
+    public static void subscribeMsg(CEPRule rule, Map<String, CEPRule> ruleMap) {
+        try {
+            IWeEventClient client = getClient(rule);
+            // subscribe topic
+            log.info("subscribe topic:{}", rule.getFromDestination());
+            String subscriptionId = client.subscribe(rule.getFromDestination(), WeEvent.OFFSET_LAST, new IWeEventClient.EventListener() {
+                @Override
+                public void onEvent(WeEvent event) {
+                    try {
+                        String content = new String(event.getContent());
+                        log.info("on event:{},content:{}", event.toString(), content);
+
+                        if (CommonUtil.checkValidJson(content)) {
+                            handleOnEvent(event, client, ruleMap);
+                        }
+                        //Analysis WeEventId  to the governance database
+                        AnalysisWeEventIdService.analysisWeEventId(rule, event.getEventId());
+                    } catch (JSONException | BrokerException e) {
+                        log.error(e.toString());
+                    }
+                }
+
+                @Override
+                public void onException(Throwable e) {
+                    log.info("on event:{}", e.toString());
+                }
+            });
+            subscriptionIdMap.put(rule.getId(), subscriptionId);
+        } catch (BrokerException e) {
+            log.info("BrokerException{}", e.toString());
+        }
+    }
+
+    public static void unSubscribeMsg(CEPRule rule, String subscriptionId) {
+        try {
+            IWeEventClient client = getClient(rule);
+            log.info("id:{},sunid:{}", rule.getId(), subscriptionId);
+            client.unSubscribe(subscriptionId);
+        } catch (BrokerException e) {
+            log.info("BrokerException{}", e.toString());
+        }
+    }
+
+    private static void sendMessageToDB(String content, CEPRule rule) {
+        JSONObject eventContent = JSONObject.parseObject(content);
+        try {
+            Connection conn = CommonUtil.getConnection(rule.getDatabaseUrl());
+
+            if (conn != null) {
+                Map<String, String> urlParamMap = CommonUtil.uRLRequest(rule.getDatabaseUrl());
+                StringBuffer insertExpression = new StringBuffer("insert into ");
+                insertExpression.append(urlParamMap.get("tableName"));
+                insertExpression.append("(");
+                StringBuffer values = new StringBuffer("values (");
+                Map<String, Integer> sqlvalue = CommonUtil.contactsql(content, rule.getPayload());
+                List<String> result = new ArrayList(sqlvalue.keySet());
+
+                //contact insert into users (first_name, last_name, date_created, is_admin, num_points)
+                for (Map.Entry<String, Integer> entry : sqlvalue.entrySet()) {
+                    System.out.println(entry.getKey() + ":" + entry.getValue());
+                    if (entry.getValue().equals(1)) {
+                        if (entry.getValue().equals(result.get(result.size() - 1))) {
+                            insertExpression.append(entry.getKey()).append(")");
+                            values.append("?）");
+                        } else {
+                            insertExpression.append(entry.getKey()).append(",");
+                            values.append("?，");
+                        }
+                    }
+                }
+
+                StringBuffer query = insertExpression.append(values);
+                log.info("query:{}", query);
+                PreparedStatement preparedStmt = conn.prepareStatement(query.toString());
+                for (int t = 0; t < result.size(); t++) {
+                    preparedStmt.setString(t + 1, eventContent.get(result.get(t)).toString());
+                }
+                // execute the preparedstatement
+                preparedStmt.execute();
+                conn.close();
+            }
+        } catch (SQLException e) {
+            log.info(e.toString());
+        }
+
+    }
+
+    public static void handleOnEvent(WeEvent event, IWeEventClient client, Map<String, CEPRule> ruleMap) {
+        log.info("handleOnEvent ruleMapsize :{}", ruleMap.size());
+
+        // get the content ,and parsing it  byte[]-->String
+        String content = new String(event.getContent());
+
+        // match the rule and send message
+        for (Map.Entry<String, CEPRule> entry : ruleMap.entrySet()) {
+            if (!StringUtils.isEmpty(entry.getValue().getPayload())
+                    && !StringUtils.isEmpty(entry.getValue().getConditionField())) {
+
+                // parsing the payload && match the content,if true and hit it
+                if (entry.getValue().getConditionType().equals(2)) {
+                    sendMessageToDB(content, entry.getValue());
+
+                } else if (hitRuleEngine(entry.getValue().getPayload(), content, entry.getValue().getConditionField())) {
+
+                    // select the field and publish the message to the toDestination
+                    try {
+                        if (entry.getValue().getConditionType().equals(1)) {
+                            // publish the message
+                            log.info("publish topic {}", entry.getValue().getSelectField());
+                            client.publish(entry.getValue().getToDestination(), content.getBytes());
+                        }
+                    } catch (BrokerException e) {
+                        log.error(e.toString());
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    private static boolean hitRuleEngine(String payload, String eventContent, String condition) {
+        if (CommonUtil.checkJson(eventContent, payload)) {
+            List<String> eventContentKeys = CommonUtil.getKeys(payload);
+            JSONObject event = JSONObject.parseObject(eventContent);
+            JexlEngine jexl = new JexlBuilder().create();
+
+            JexlContext context = new MapContext();
+            for (String key : eventContentKeys) {
+                context.set(key, event.get(key));
+            }
+            // Create an expression  "a>10"
+            return (Boolean) jexl.createExpression(condition).evaluate(context);
+        }
+        return Boolean.FALSE;
+    }
+}
+
