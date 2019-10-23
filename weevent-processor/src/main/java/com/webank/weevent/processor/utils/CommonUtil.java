4492852510db4d6395119acc61b29fbfa1ddf517
Cristic <CristicMay@gmail.com>
2019-10-23 21:42:30 +0800

    issue#104: add the processor module (#262)

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/utils/CommonUtil.java
@@ -0,0 +1,175 @@
+package com.webank.weevent.processor.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+public class CommonUtil {
+
+
+    /**
+     * check the database url
+     *
+     * @param databaseUrl data bae url
+     * @return connection
+     */
+
+    public static Connection getConnection(String databaseUrl) {
+        String driver = "com.mysql.jdbc.Driver";
+        try {
+            Class.forName(driver);
+            return DriverManager.getConnection(databaseUrl);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static String urlPage(String url) {
+        String page = null;
+        String[] arrSplit;
+
+        String urlTemp = url.trim().toLowerCase();
+
+        arrSplit = urlTemp.split("[?]");
+        if ((urlTemp.length()) > 0 && (arrSplit.length >=1) && (arrSplit[0] != null)) {
+            page = arrSplit[0];
+        }
+
+        return page;
+    }
+
+
+    private static String truncateUrlPage(String strURL) {
+        String strAllParam = null;
+        String url = strURL.trim().toLowerCase();
+
+        String[] arrSplit = url.split("[?]");
+        if ((url.length() > 1) && (arrSplit.length) > 1 && (arrSplit[1] != null)) {
+            strAllParam = arrSplit[1];
+        }
+
+        return strAllParam;
+    }
+
+
+    public static Map<String, String> uRLRequest(String URL) {
+        Map<String, String> mapRequest = new HashMap<String, String>();
+
+        String[] arrSplit = null;
+
+        String strUrlParam = truncateUrlPage(URL);
+        if (strUrlParam == null) {
+            return mapRequest;
+        }
+        arrSplit = strUrlParam.split("[&]");
+        for (String strSplit : arrSplit) {
+            String[] arrSplitEqual = null;
+            arrSplitEqual = strSplit.split("[=]");
+
+            if (arrSplitEqual.length > 1) {
+                mapRequest.put(arrSplitEqual[0], arrSplitEqual[1]);
+
+            } else {
+                if (!arrSplitEqual[0].equals("")) {
+                    mapRequest.put(arrSplitEqual[0], "");
+                }
+            }
+        }
+        return mapRequest;
+    }
+
+
+    public static List<String> getKeys(String objJson) {
+        List<String> keys = new ArrayList<>();
+        try {
+            if (checkValidJson(objJson)) {
+                for (Map.Entry<String, Object> entry : JSONObject.parseObject(objJson).entrySet()) {
+                    keys.add(entry.getKey());
+                }
+            } else {
+                keys = null;
+            }
+
+        } catch (JSONException e) {
+            keys = null;
+            log.info("json get key error");
+        }
+        return keys;
+    }
+
+    /**
+     * check valid json string
+     *
+     * @param test json string
+     * @return true or false
+     */
+    public final static boolean checkValidJson(String test) {
+        try {
+            JSONObject.parseObject(test);
+        } catch (JSONException ex) {
+            try {
+                JSONObject.parseArray(test);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkJson(String content, String objJson) {
+        boolean tag = true;
+        //parsing and match
+        if (!StringUtils.isEmpty(content)
+                && !StringUtils.isEmpty(objJson)) {
+            List<String> contentKeys = getKeys(content);
+            List<String> objJsonKeys = getKeys(objJson);
+
+            // objJsonKeys must longer than the contentKeys
+            if (contentKeys.size() > objJsonKeys.size()) {
+                tag = false;
+            } else {
+                for (String contentKey : contentKeys) {
+                    if (!objJsonKeys.contains(contentKey)) {
+                        tag = false;
+                    }
+                }
+            }
+        }
+        log.info("checkJson tag:{}", tag);
+        return tag;
+    }
+
+    public static Map<String, Integer> contactsql(String content, String objJson) {
+        Map<String, Integer> sql = new HashMap<>();
+        if (!StringUtils.isEmpty(content)
+                && !StringUtils.isEmpty(objJson)) {
+            List<String> objJsonKeys = getKeys(objJson);
+            for (String key : objJsonKeys) {
+                sql.put(key, 0);
+                if (!content.contains(key)) {
+                    sql.put(key, 1);
+                }
+            }
+        }
+        return sql;
+    }
+
+}
+
+
+
