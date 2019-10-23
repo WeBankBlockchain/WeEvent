3d8902abc6b75be58175a7a1bcd2f3fc992f3cb0
matthewliu <saintping@gmail.com>
2019-10-23 19:02:49 +0800

    Revert "Revert "fix issue #104 , add weevent processor module (#247)" (#259)"
    This reverts commit 915bf0fa00a1acc3a9be0eaff09322702215fbb0.

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/utils/RetCode.java
@@ -0,0 +1,45 @@
+package com.webank.weevent.processor.utils;
+
+/**
+ * REST服务端的返回类
+ */
+public class RetCode {
+    private int errorCode;
+    private String errorMsg;
+
+    private RetCode(int errorCode, String errorMsg) {
+        this.errorCode = errorCode;
+        this.errorMsg = errorMsg;
+    }
+
+    public static RetCode mark(int errorCode, String errorMsg) {
+        return new RetCode(errorCode, errorMsg);
+    }
+
+    public static RetCode mark(Integer errorCode) {
+        return new RetCode(errorCode, null);
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
+    }
+
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    @Override
+    public String toString() {
+        return "RetCode [errorCode=" + errorCode + ", errorMsg=" + errorMsg + "]";
+    }
+
+
+}
