1612d8a5ebfa936475633de9066e0d681601dcc5
Cristic <CristicMay@gmail.com>
2019-10-23 21:42:30 +0800

    issue#104: add the processor module (#262)

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/utils/ObjectTranscoder.java
@@ -0,0 +1,76 @@
+package com.webank.weevent.processor.utils;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ObjectTranscoder {
+
+    private static ObjectTranscoder obj = new ObjectTranscoder();
+
+    private ObjectTranscoder() {
+
+    }
+
+    public static ObjectTranscoder getInstance() {
+        return obj;
+    }
+
+    public byte[] serialize(Object value) {
+        if (value == null) {
+            throw new NullPointerException("Can't serialize null");
+        }
+        byte[] rv = null;
+        ByteArrayOutputStream bos = null;
+        ObjectOutputStream os = null;
+        try {
+            bos = new ByteArrayOutputStream();
+            os = new ObjectOutputStream(bos);
+            os.writeObject(value);
+            os.close();
+            bos.close();
+            rv = bos.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (os != null) os.close();
+                if (bos != null) bos.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return rv;
+    }
+
+    public Object deserialize(byte[] in) {
+        Object rv = null;
+        ByteArrayInputStream bis = null;
+        ObjectInputStream is = null;
+        try {
+            if (in != null) {
+                bis = new ByteArrayInputStream(in);
+                is = new ObjectInputStream(bis);
+                rv = is.readObject();
+                is.close();
+                bis.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (is != null) is.close();
+                if (bis != null) bis.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return rv;
+    }
+
+}
