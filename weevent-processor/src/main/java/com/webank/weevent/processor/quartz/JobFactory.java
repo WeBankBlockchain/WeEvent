6269fa681cdc820029e4b250fb2d33505b4c4ed1
Cristic <CristicMay@gmail.com>
2019-10-23 21:42:30 +0800

    issue#104: add the processor module (#262)

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/quartz/JobFactory.java
@@ -0,0 +1,30 @@
+package com.webank.weevent.processor.quartz;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.scheduling.quartz.AdaptableJobFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobFactory extends AdaptableJobFactory {
+    /**
+     * AutowireCapableBeanFactory
+     *
+     */
+    private AutowireCapableBeanFactory factory;
+
+    public JobFactory(AutowireCapableBeanFactory factory) {
+        this.factory = factory;
+    }
+
+    /**
+     * create the job
+     */
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+
+        Object job = super.createJobInstance(bundle);
+        factory.autowireBean(job);
+        return job;
+    }
+}
