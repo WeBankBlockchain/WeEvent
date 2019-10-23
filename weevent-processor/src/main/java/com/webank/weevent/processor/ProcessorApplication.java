a69d17b438f46d9c8b5c92f821cb3cab8f008b20
Cristic <CristicMay@gmail.com>
2019-10-23 21:42:30 +0800

    issue#104: add the processor module (#262)

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/ProcessorApplication.java
@@ -0,0 +1,69 @@
+package com.webank.weevent.processor;
+
+import com.webank.weevent.processor.cache.CEPRuleCache;
+import com.webank.weevent.processor.config.ProcessorConfig;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Slf4j
+@SpringBootApplication
+@MapperScan(basePackages = "com.webank.weevent.processor")
+public class ProcessorApplication {
+    public static ProcessorConfig processorConfig;
+    public static ApplicationContext applicationContext;
+    @Autowired
+    public void setContext(ApplicationContext context) {
+        applicationContext = context;
+    }
+
+    public static void main(String[] args) {
+        log.info("start processor success");
+        SpringApplication app = new SpringApplication(ProcessorApplication.class);
+        app.addListeners(new ApplicationPidFileWriter());
+        app.run(args);
+        log.info("start processor success");
+    }
+
+    @Autowired
+    public void setProcessorConfig(ProcessorConfig config) {
+        processorConfig = config;
+    }
+
+    private static void exit() {
+        if (applicationContext != null) {
+            System.exit(SpringApplication.exit(applicationContext));
+        } else {
+            System.exit(1);
+        }
+    }
+
+
+    @Bean(name = "processor_daemon_task_executor")
+    public static ThreadPoolTaskExecutor getThreadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        pool.setThreadNamePrefix("processor_daemon_");
+        // run in thread immediately, no blocking queue
+        pool.setQueueCapacity(0);
+        pool.setDaemon(true);
+        pool.initialize();
+
+        log.info("init processor daemon thread pool");
+        return pool;
+    }
+
+    @Bean
+    public CEPRuleCache cEPRuleCache() {
+        log.info("cEPRuleCache....");
+        return new CEPRuleCache();
+    }
+}
+
+
---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/model/CEPRule.java
@@ -0,0 +1,222 @@
+package com.webank.weevent.processor.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CEPRule implements Serializable {
+    private String id;
+
+    private String ruleName;
+
+    private String fromDestination;
+
+    private String brokerUrl;
+
+    private String payload;
+
+    private Integer payloadType;
+
+    private String selectField;
+
+    private String conditionField;
+
+    private Integer conditionType;
+
+    private String toDestination;
+
+    private String databaseUrl;
+
+    private Date createdTime;
+
+    private Integer status;
+
+    private String errorDestination;
+
+    private String errorCode;
+
+    private String errorMessage;
+
+    private Date updatedTime;
+
+    private String brokerId;
+
+    private String userId;
+
+    public CEPRule(String id, String ruleName, String fromDestination, String brokerUrl, String payload, Integer payloadType, String selectField, String conditionField, Integer conditionType, String toDestination, String databaseUrl, Date createdTime, Integer status, String errorDestination, String errorCode, String errorMessage, Date updatedTime, String brokerId, String userId) {
+        this.id = id;
+        this.ruleName = ruleName;
+        this.fromDestination = fromDestination;
+        this.brokerUrl = brokerUrl;
+        this.payload = payload;
+        this.payloadType = payloadType;
+        this.selectField = selectField;
+        this.conditionField = conditionField;
+        this.conditionType = conditionType;
+        this.toDestination = toDestination;
+        this.databaseUrl = databaseUrl;
+        this.createdTime = createdTime;
+        this.status = status;
+        this.errorDestination = errorDestination;
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.updatedTime = updatedTime;
+        this.brokerId = brokerId;
+        this.userId = userId;
+    }
+
+    public CEPRule() {
+        super();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getRuleName() {
+        return ruleName;
+    }
+
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName == null ? null : ruleName.trim();
+    }
+
+    public String getFromDestination() {
+        return fromDestination;
+    }
+
+    public void setFromDestination(String fromDestination) {
+        this.fromDestination = fromDestination == null ? null : fromDestination.trim();
+    }
+
+    public String getBrokerUrl() {
+        return brokerUrl;
+    }
+
+    public void setBrokerUrl(String brokerUrl) {
+        this.brokerUrl = brokerUrl == null ? null : brokerUrl.trim();
+    }
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload == null ? null : payload.trim();
+    }
+
+    public Integer getPayloadType() {
+        return payloadType;
+    }
+
+    public void setPayloadType(Integer payloadType) {
+        this.payloadType = payloadType;
+    }
+
+    public String getSelectField() {
+        return selectField;
+    }
+
+    public void setSelectField(String selectField) {
+        this.selectField = selectField == null ? null : selectField.trim();
+    }
+
+    public String getConditionField() {
+        return conditionField;
+    }
+
+    public void setConditionField(String conditionField) {
+        this.conditionField = conditionField == null ? null : conditionField.trim();
+    }
+
+    public Integer getConditionType() {
+        return conditionType;
+    }
+
+    public void setConditionType(Integer conditionType) {
+        this.conditionType = conditionType;
+    }
+
+    public String getToDestination() {
+        return toDestination;
+    }
+
+    public void setToDestination(String toDestination) {
+        this.toDestination = toDestination == null ? null : toDestination.trim();
+    }
+
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+
+    public void setDatabaseUrl(String databaseUrl) {
+        this.databaseUrl = databaseUrl == null ? null : databaseUrl.trim();
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getErrorDestination() {
+        return errorDestination;
+    }
+
+    public void setErrorDestination(String errorDestination) {
+        this.errorDestination = errorDestination == null ? null : errorDestination.trim();
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode == null ? null : errorCode.trim();
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage == null ? null : errorMessage.trim();
+    }
+
+    public Date getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(Date updatedTime) {
+        this.updatedTime = updatedTime;
+    }
+
+    public String getBrokerId() {
+        return brokerId;
+    }
+
+    public void setBrokerId(String brokerId) {
+        this.brokerId = brokerId == null ? null : brokerId.trim();
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+}
