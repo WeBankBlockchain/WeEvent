3103e7721715648960c31c573a8b9e23c740c014
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-10-24 11:36:43 +0800

    beautify

--- weevent-processor/src/main/java/com/webank/weevent/processor/quartz/QuartzConfig.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/quartz/QuartzConfig.java
@@ -31,15 +31,18 @@ public class QuartzConfig {
                 String fileUtl = this.getClass().getClassLoader().getResource("processor.properties").getPath();
                 FileInputStream in = new FileInputStream(fileUtl);
                 Properties quartzPropertie = new Properties();
-
-                quartzPropertie.setProperty(ConstantsHelper.jobStoreClass,ConstantsHelper.JobStoreTX);
-                quartzPropertie.setProperty("org.quartz.jobStore.driverDelegateClass","org.quartz.impl.jdbcjobstore.StdJDBCDelegate");
+                quartzPropertie.load(in);
+                quartzPropertie.setProperty("org.quartz.jobStore.class","org.quartz.impl.jdbcjobstore.JobStoreTX");
                 quartzPropertie.setProperty("org.quartz.jobStore.isClustered","true");
                 quartzPropertie.setProperty("org.quartz.threadPool.class","org.quartz.simpl.SimpleThreadPool");
+                quartzPropertie.setProperty("org.quartz.jobStore.driverDelegateClass","org.quartz.impl.jdbcjobstore.StdJDBCDelegate");
                 quartzPropertie.setProperty("org.quartz.threadPool.makeThreadsDaemons","true");
+                quartzPropertie.setProperty("org.quartz.threadPool.threadsInheritContextClassLoaderOfInitializingThread","true");
                 quartzPropertie.setProperty("org.quartz.plugin.shutdownHook.cleanShutdown","true");
                 quartzPropertie.setProperty("org.quartz.plugin.shutdownHook.class","org.quartz.plugins.management.ShutdownHookPlugin");
-                quartzPropertie.load(in);
+                quartzPropertie.setProperty("org.quartz.scheduler.instanceId","auto");
+                quartzPropertie.setProperty("org.quartz.scheduler.skipupdateCheck","auto");
+                quartzPropertie.setProperty("org.quartz.dataSource.weevent_processor.driver","com.mysql.jdbc.Driver");
                 factory.setQuartzProperties(quartzPropertie);
                 factory.setJobFactory(jobFactory);
                 in.close();
