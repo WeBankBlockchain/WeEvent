3cbfc13aaf080df3c842a4f7be48e1b37a0a48c5
anyspa <35908094+anyspa@users.noreply.github.com>
2019-10-28 16:41:48 +0800

    optimize deploy-fabric-topic-control.sh to support reentrance (#278)

--- weevent-broker/dist/deploy-fabric-topic-control.sh
+++ weevent-broker/dist/deploy-fabric-topic-control.sh
@@ -6,16 +6,31 @@ if [[ -z ${JAVA_HOME} ]];then
    exit 1
 fi
 
+if [ $# -eq 0 ];then
+    echo "Usage:"
+    echo "    sh deploy-fabric-topic-control deploy|add|update"
+    exit 1
+fi
+
 java_cmd_deploy="${JAVA_HOME}/bin/java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.path=./lib,../lib -Dloader.main=com.webank.weevent.broker.fabric.util.FabricDeployContractUtil org.springframework.boot.loader.PropertiesLauncher"
 java_cmd_invoke="${JAVA_HOME}/bin/java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.path=./lib,../lib -Dloader.main=com.webank.weevent.broker.fabric.util.FabricUpdateContractUtil org.springframework.boot.loader.PropertiesLauncher"
 cmd=$1
 
 if [[ ${cmd} = "deploy" ]];then
+    echo "begin deploy topic and topicController contract. "
     ${java_cmd_deploy}
     if [[ $? -ne 0 ]];then
-        echo "deploy topic control failed."
+        echo "deploy topic and topicController contract failed."
         exit 1
     fi
+
+    echo "begin add topic into topicController contract."
+    ${java_cmd_invoke} add
+    if [[ $? -ne 0 ]];then
+        echo "add topic into topicController contract failed."
+        exit 1
+    fi
+    echo "deploy contract success."
 fi
 
 if [[ ${cmd} = "add" || ${cmd} = "update" ]];then
