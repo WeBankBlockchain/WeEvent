0d36e4d4ad115c3b1b5afbf3f196aa6587e66c55
Cristic <CristicMay@gmail.com>
2019-10-30 17:51:51 +0800

    add the check expression function (#285)

--- weevent-processor/src/test/java/com/webank/weevent/processor/SqlParserTest.java
+++ weevent-processor/src/test/java/com/webank/weevent/processor/SqlParserTest.java
@@ -1,5 +1,10 @@
 package com.webank.weevent.processor;
 
+import java.util.List;
+
+import com.webank.weevent.processor.utils.CommonUtil;
+
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.jexl3.JexlBuilder;
 import org.apache.commons.jexl3.JexlContext;
@@ -27,16 +32,43 @@ public class SqlParserTest {
 
     @Test
     public void basicExpressionSingle() {
-        JexlEngine jexl = new JexlBuilder().create();
-        JexlContext context = new MapContext();
-        context.set("a", 11);
-        context.set("b", 1);
-        context.set("c", 1);
-        // Create an expression
-        String jexlExp = "a>10";
-        Boolean e = (Boolean) jexl.createExpression(jexlExp).evaluate(context);
-        log.info(e.toString());
-        Assert.assertEquals(true, e);
+        try {
+            JexlEngine jexl = new JexlBuilder().create();
+            JexlContext context = new MapContext();
+            context.set("a", "rere");
+            context.set("b", 1);
+            context.set("c", 1);
+            // Create an expression
+            String jexlExp = "a>10";
+            Boolean e = (Boolean) jexl.createExpression(jexlExp).evaluate(context);
+            log.info(e.toString());
+            Assert.assertEquals(true, e);
+        } catch (Exception e) {
+            log.info("error number");
+        }
+
+    }
+
+    @Test
+    public void basicCheckExpression() {
+        try {
+            String payload = "{\"a\":1,\"b\":\"test\",\"c\":10}";
+            String condition = "c<10";
+            List<String> payloadContentKeys = CommonUtil.getKeys(payload);
+            JSONObject payloadJson = JSONObject.parseObject(payload);
+            JexlEngine jexl = new JexlBuilder().create();
+
+            JexlContext context = new MapContext();
+            for (String key : payloadContentKeys) {
+                context.set(key, payloadJson.get(key));
+            }
+            Boolean e = (Boolean) jexl.createExpression(condition).evaluate(context);
+            log.info(e.toString());
+            Assert.assertEquals(true, e);
+        } catch (Exception e) {
+            log.info("error number");
+        }
+
     }
 
 }
