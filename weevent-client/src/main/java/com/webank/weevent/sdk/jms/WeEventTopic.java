c3905e1e883f2c85560d92a72a23f1010abd3738
matthewliu <saintping@gmail.com>
2019-11-06 15:41:02 +0800

    issue #265: fix bug in publish event via stomp from java client (#308)

--- weevent-client/src/main/java/com/webank/weevent/sdk/jms/WeEventTopic.java
+++ weevent-client/src/main/java/com/webank/weevent/sdk/jms/WeEventTopic.java
@@ -1,6 +1,8 @@
 package com.webank.weevent.sdk.jms;
 
 
+import java.util.Map;
+
 import javax.jms.JMSException;
 import javax.jms.Topic;
 
@@ -14,38 +16,47 @@ public class WeEventTopic implements Topic {
     private String topicName;
     private String offset;
     private String groupId;
+    private Map<String, String> extensions;
     private String continueSubscriptionId;
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
 
     public WeEventTopic(String topicName) {
         this.topicName = topicName;
     }
 
+    @Override
+    public String getTopicName() throws JMSException {
+        return this.topicName;
+    }
+
     public String getOffset() {
-        return offset;
+        return this.offset;
     }
 
     public void setOffset(String offset) {
         this.offset = offset;
     }
 
+    public String getGroupId() {
+        return this.groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    public Map<String, String> getExtensions() {
+        return this.extensions;
+    }
+
+    public void setExtensions(Map<String, String> extensions) {
+        this.extensions = extensions;
+    }
+
     public String getContinueSubscriptionId() {
-        return continueSubscriptionId;
+        return this.continueSubscriptionId;
     }
 
     public void setContinueSubscriptionId(String continueSubscriptionId) {
         this.continueSubscriptionId = continueSubscriptionId;
-    }
-
-    @Override
-    public String getTopicName() throws JMSException {
-        return this.topicName;
     }
 }
