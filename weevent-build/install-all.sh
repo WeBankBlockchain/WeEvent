9fae3316cf80806e1de3a15eb4590872b3259dfa
v_wbyqli <4f4x2X6#>
2019-11-06 19:38:28 +0800

    rollback file

--- weevent-build/install-all.sh
+++ weevent-build/install-all.sh
@@ -1,105 +1,243 @@
 #!/bin/bash
+#
+# install WeEvent service, support FISCO-BCOS 1.3 and 2.0
+#
+################################################################################
 
-function processor_setup() {
-    echo "install processor into ${out_path} "
+java_home_path=
+out_path=""
+block_chain_type=
+block_chain_version=
+block_chain_channel=
+block_chain_node_path=
+fabric_orderer_address=
+fabric_peer_address=
+broker_port=7000
 
-    #create processor list
-    mkdir -p ${out_path}
-    cp -r ./* ${out_path}/
-    rm -f ${out_path}/install-processor.sh
+nginx_port=8080
 
-    if [[ -z ${server_port} ]];then
-        echo "server_port is empty."
-        echo "set server_port failed"
-        exit 1
-    else
-       sed -i "/server.port*/cserver.port=${server_port}" ${out_path}/conf/application-prod.properties
-    fi
-    echo "set server_port success"
+governance_port=
+mysql_ip=
+mysql_port=
+mysql_user=
+mysql_password=
 
-    if [[ -z ${mysql_ip} ]];then
-        echo "mysql_ip is empty."
-        echo "set mysql_ip failed"
-        exit 1
-    else
-       sed -i "s/127.0.0.1:3306/${mysql_ip}:3306/" ${out_path}/conf/application-prod.properties
-       sed -i "s/127.0.0.1:3306/${mysql_ip}:3306/" ${out_path}/conf/processor.properties
-    fi
-    echo "set mysql_ip success"
-
-    if [[ -z ${mysql_port} ]];then
-        echo "mysql_port is empty."
-        echo "set mysql_port failed"
-        exit 1
-    else
-       sed -i "s/3306/${mysql_port}/" ${out_path}/conf/application-prod.properties
-       sed -i "s/3306/${mysql_port}/" ${out_path}/conf/processor.properties
-    fi
-    echo "set mysql_port success"
-
-    if [[ -z ${mysql_user} ]];then
-        echo "mysql_user is empty."
-        echo "set mysql_user failed"
-        exit 1
-    else
-       sed -i "s/xxxx/${mysql_user}/" ${out_path}/conf/application-prod.properties
-       sed -i "s/xxxx/${mysql_user}/" ${out_path}/conf/processor.properties
-    fi
-    echo "set mysql_user success"
-
-    if [[ -z ${mysql_pwd} ]];then
-        echo "mysql_pwd is empty"
-        echo "set mysql_pwd failed"
-        exit 1
-    else
-       sed -i "s/yyyy/${mysql_pwd}/" ${out_path}/conf/application-prod.properties
-       sed -i "s/yyyy/${mysql_pwd}/" ${out_path}/conf/processor.properties
-    fi
-    echo "set mysql_pwd success"
+processor_port=
 
 
-    # init db, create database and tables
-    cd ${out_path}
-    ./init-processor.sh
-    if [[ $? -ne 0 ]];then
-        echo "Error,init mysql fail"
-        exit 1
-    fi
-    echo "init db success"
+current_path=$PWD
 
-    echo "processor module install success"
+function yellow_echo (){
+    local what=$*
+    echo -e "\e[1;33m${what} \e[0m"
 }
 
-#get parameter
-para=""
+function error_message(){
+    local message=$1
+    echo "ERROR - ${message}"
+    exit 1
+}
 
-# usage
+function properties_get(){
+    local file="config.properties"
+    local param=$1
+    value=$(grep -v '#' ${file} | grep ${param} | cut -d'=' -f2 | sed 's/\r//' | awk '$1=$1')
+    if [[ -z "$value" ]]; then
+        echo "error_message"
+        error_message "ERROR config.properties get param $param failed."
+        exit 1
+    fi
+    echo ${value}
+}
+
+function set_global_param(){
+    java_home_path=$(properties_get "JAVA_HOME")
+    block_chain_type=$(properties_get "blockchain.type")
+    if [[ ${block_chain_type} == "fisco" ]];then
+        block_chain_version=$(properties_get "fisco-bcos.version")
+        block_chain_channel=$(properties_get "fisco-bcos.channel")
+        block_chain_node_path=$(properties_get  "fisco-bcos.node_path")
+        if [[ "${block_chain_node_path:0:1}" == "~" ]];then
+            block_chain_node_path=$(realpath -m ${HOME}/${block_chain_node_path:1})
+        else
+            block_chain_node_path=$(realpath -m ${block_chain_node_path})
+        fi
+    else
+        fabric_orderer_address=$(properties_get "chain.orderer.address")
+        fabric_peer_address=$(properties_get "chain.peer.address")
+    fi
+    nginx_port=$(properties_get "nginx.port")
+
+    broker_port=$(properties_get "broker.port")
+
+    governance_enable=$(properties_get  "governance.enable")
+    governance_port=$(properties_get "governance.port")
+
+    mysql_ip=$(properties_get "governance.mysql.ip")
+    mysql_port=$(properties_get  "governance.mysql.port")
+    mysql_user=$(properties_get "governance.mysql.user")
+    mysql_password=$(properties_get "governance.mysql.password")
+
+    processor_enable=$(properties_get  "processor.enable")
+    processor_port=$(properties_get "processor.port")
+}
+
+function check_port(){
+    lsof -i:${1}
+    if [[ $? -eq 1 ]];then
+        echo "$1 port is okay"
+    else
+        echo "$1 port is in used"
+        exit 1
+    fi
+}
+
+function check_telnet(){
+    eval $(echo $1 | awk '{split($0, filearray, ";");for(i in filearray) print "arr["i"]="filearray[i]}')
+    for channel in ${arr[*]}
+    do
+        local channel_ip=$(echo | awk '{split("'${channel}'", array, ":");print array[1]}')
+        local channel_port=$(echo | awk '{split("'${channel}'", array, ":");print array[2]}')
+        ssh ${channel_ip} -p ${channel_port} -o ConnectTimeout=3 2>&1 | grep "Connection refused" &>> ${current_path}/install.log
+        if [[ $? -eq 0 ]];then
+            echo "${channel} connection fail"
+            exit 1
+        fi
+    done
+}
+
+function check_param(){
+    if [[ ! -d ${java_home_path} ]]; then
+        echo "JAVA_HOME path not exist, ${java_home_path}"
+        exit 1;
+    fi
+
+    check_port ${broker_port}
+    check_port ${nginx_port}
+    if [[ ${governance_enable} = "true" ]];then
+        check_telnet ${mysql_ip}:${mysql_port}
+    fi
+    if [[ ${block_chain_type} == "fisco" ]];then
+        if [[ -d ${block_chain_node_path} ]]; then
+            check_telnet ${block_chain_channel}
+            echo "param ok"
+        else
+            echo "path not exist, ${block_chain_node_path}"
+            exit 1
+        fi
+    fi
+}
+
+### check the result and print it
+function check_result(){
+    if [[ $? -eq 0 ]];then
+        yellow_echo "$1 success"
+    else
+        yellow_echo "$1 failed, exit"
+        exit 1
+   fi
+}
+
+function install_module(){
+    yellow_echo "install module broker"
+    cd ${current_path}/modules/broker
+    if [[ ${block_chain_type} == "fisco" ]];then
+        ./install-broker.sh --out_path ${out_path}/broker --listen_port ${broker_port} --block_chain_type ${block_chain_type} --block_chain_node_path ${block_chain_node_path} --channel_info ${block_chain_channel} --version ${block_chain_version}
+    else
+        ./install-broker.sh --out_path ${out_path}/broker --listen_port ${broker_port} --block_chain_type ${block_chain_type} --fabric_orderer_address ${fabric_orderer_address} --fabric_peer_address ${fabric_peer_address}
+    fi
+    check_result "install broker"
+
+    yellow_echo "install module nginx"
+    cd ${current_path}/modules/nginx
+    ./install-nginx.sh --nginx_path ${out_path}/nginx --nginx_port ${nginx_port} --broker_port ${broker_port} --governance_port ${governance_port} &>> ${current_path}/install.log
+    check_result "install nginx"
+
+    if [[ ${governance_enable} = "true" ]];then
+        yellow_echo "install module governance"
+        cd ${current_path}/modules/governance
+        ./install-governance.sh --out_path ${out_path}/governance --server_port ${governance_port} --mysql_ip ${mysql_ip} --mysql_port ${mysql_port} --mysql_user ${mysql_user} --mysql_pwd ${mysql_password} &>> ${current_path}/install.log
+        check_result "install governance"
+    fi
+    if [[ ${processor_enable} = "true" ]];then
+        yellow_echo "install module processor"
+        cd ${current_path}/modules/processor
+        ./install-processor.sh --out_path ${out_path}/processor --server_port ${processor_port} --mysql_ip ${mysql_ip} --mysql_port ${mysql_port} --mysql_user ${mysql_user} --mysql_pwd ${mysql_password} &>> ${current_path}/install.log
+        check_result "install processor"
+    fi
+
+}
+
+function config_java_home(){
+    if [[ -e ${current_path}/modules/broker/broker.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/broker/broker.sh
+    fi
+    if [[ -e ${current_path}/modules/broker/deploy-topic-control.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/broker/deploy-topic-control.sh
+    fi
+    if [[ -e ${current_path}/modules/broker/deploy-fabric-topic-control.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/broker/deploy-fabric-topic-control.sh
+    fi
+    if [[ -e ${current_path}/modules/governance/governance.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/governance/governance.sh
+    fi
+    if [[ -e ${current_path}/modules/governance/init-governance.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/governance/init-governance.sh
+    fi
+    if [[ -e ${current_path}/modules/processor/processor.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/processor/processor.sh
+    fi
+    if [[ -e ${current_path}/modules/processor/init-processor.sh ]];then
+        sed -i "/JAVA_HOME=/cJAVA_HOME=${java_home_path}" ${current_path}/modules/processor/init-processor.sh
+    fi
+}
+
+function update_server_port(){
+    sed -i "s/8080/$nginx_port/g" ${current_path}/bin/check-service.sh
+}
+
+function main(){
+    # confirm
+    if [[ -d $2 ]]; then
+        read -p "$2 already exist, continue? [Y/N]" cmd_input
+        if [[ "Y" != "$cmd_input" && "y" != "$cmd_input" ]]; then
+            echo "input $cmd_input, install skipped"
+            exit 1
+        fi
+    fi
+    mkdir -p $2
+    if [[ $? -ne 0 ]];then
+        echo "create path $2 fail !!! "
+        exit 1
+    fi
+    out_path=$(realpath $2)
+
+    # set the params
+    set_global_param
+
+    # check the dir is exist or not
+    check_param
+
+    # set the check service port
+    update_server_port
+
+    # set the JAVA_HOME
+    config_java_home
+
+    # copy jar
+    cp -r ${current_path}/modules/lib/ ${out_path}
+
+    # install module
+    install_module
+
+    cp ${current_path}/bin/* ${out_path}
+}
+
+# Usage message
 if [[ $# -lt 2 ]]; then
     echo "Usage:"
-    echo "    $0 --out_path /data/app/weevent-install/processor "
-    echo "      --server_port --mysql_ip --mysql_port  --mysql_user  --mysql_pwd  "
+    echo "    ./install-all.sh -p target_install_path"
     exit 1
 fi
 
-server_port=""
-mysql_ip=""
-mysql_port=""
-mysql_user=""
-mysql_pwd=""
-out_path=""
-current_path=$(pwd)
-echo "current path $current_path"
-
-while [[ $# -ge 2 ]] ; do
-    case "$1" in
-        --out_path) para="$1 = $2;";out_path="$2";shift 2;;
-        --server_port) para="$1 = $2;";server_port="$2";shift 2;;
-        --mysql_ip) para="$1 = $2;";mysql_ip="$2";shift 2;;
-        --mysql_port) para="$1 = $2;";mysql_port="$2";shift 2;;
-        --mysql_user) para="$1 = $2;";mysql_user="$2";shift 2;;
-        --mysql_pwd) para="$1 = $2;";mysql_pwd="$2";shift 2;;
-        *) echo "unknown parameter $1." ; exit 1 ; break;;
-    esac
-done
-
-processor_setup
+main $1 $2
