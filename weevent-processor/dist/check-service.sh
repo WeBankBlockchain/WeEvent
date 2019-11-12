44c88d5b463dc3aff36ea9c2c80604a12b197363
anyspa <35908094+anyspa@users.noreply.github.com>
2019-11-12 20:41:56 +0800

    fix check-service bash script , replace char ':' to '=' (#343)

--- weevent-processor/dist/check-service.sh
+++ weevent-processor/dist/check-service.sh
@@ -10,7 +10,7 @@ function check_processor(){
         exit 1
     fi
 
-    port=$(grep "port" ${current_path}/conf/application-prod.properties| head -1 | awk -F':' '{print $NF}' | sed s/[[:space:]]//g)
+    port=$(grep "port" ${current_path}/conf/application-prod.properties| head -1 | awk -F'=' '{print $NF}' | sed s/[[:space:]]//g)
     if [[ $? -ne 0 ]];then
         echo "get processor port fail"
         exit 1
