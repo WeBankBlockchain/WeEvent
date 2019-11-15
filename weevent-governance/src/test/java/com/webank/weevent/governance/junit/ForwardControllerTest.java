c56164e840804b1243e49544c37b60a413d31d38
puremilkfan <32903155+puremilkfan@users.noreply.github.com>
2019-11-15 21:03:47 +0800

    fix issue #358 : detect rule cyclic in DAG (#359)

---
+++ weevent-governance/src/test/java/com/webank/weevent/governance/junit/ForwardControllerTest.java
@@ -0,0 +1,61 @@
+package com.webank.weevent.governance.junit;
+
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+
+import com.webank.weevent.governance.JUnitTestBase;
+import com.webank.weevent.governance.properties.ConstantProperties;
+
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@Slf4j
+public class ForwardControllerTest extends JUnitTestBase {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    private Cookie cookie;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        this.cookie = new Cookie(ConstantProperties.COOKIE_MGR_ACCOUNT_ID, "1");
+
+    }
+
+    @Before
+    public void before() {
+        log.info("=============================={}.{}==============================",
+                this.getClass().getSimpleName(),
+                this.testName.getMethodName());
+    }
+
+
+    @Test
+    public void testGroupList() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(MockMvcRequestBuilders.get("/weevent/" + "admin" + "/" + "listGroup").contentType(MediaType.APPLICATION_JSON_UTF8).cookie(this.cookie))
+                .andReturn().getResponse();
+        String contentAsString = response.getContentAsString();
+        Assert.assertEquals(response.getStatus(), 200);
+        Assert.assertNotNull(contentAsString);
+        JSONObject jsonObject = JSONObject.parseObject(contentAsString);
+        Object code = jsonObject.get("code");
+        Object data = jsonObject.get("data");
+        Assert.assertEquals(0, code);
+        Assert.assertTrue(data instanceof List);
+    }
+}
