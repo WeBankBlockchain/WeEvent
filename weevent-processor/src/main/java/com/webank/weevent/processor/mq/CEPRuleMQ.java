1952d25f804894fb2836bc49c90b9b5be15a6feb
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-11-15 14:42:58 +0800

    remove useless code

--- weevent-processor/src/main/java/com/webank/weevent/processor/mq/CEPRuleMQ.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/mq/CEPRuleMQ.java
@@ -4,7 +4,6 @@ import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.BlockingDeque;
