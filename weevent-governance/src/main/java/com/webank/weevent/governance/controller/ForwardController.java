193acae2946eb3df19407cfa641fc5e612a53b1d
puremilkfan <32903155+puremilkfan@users.noreply.github.com>
2019-11-15 21:03:47 +0800

    fix issue #358 : detect rule cyclic in DAG (#359)

--- weevent-governance/src/main/java/com/webank/weevent/governance/controller/ForwardController.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/controller/ForwardController.java
@@ -1,48 +1,45 @@
 package com.webank.weevent.governance.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.webank.weevent.governance.exception.GovernanceException;
+import com.webank.weevent.governance.service.CommonService;
+
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @Slf4j
+@RestController
 public class ForwardController {
 
+
     @Autowired
-    private RestTemplate restTemplate;
+    private CommonService commonService;
 
     @Value("${weevent.url}")
     private String url;
 
     @RequestMapping(value = "/weevent/{path1}/{path2}", method = RequestMethod.GET)
-    public Object forward(@PathVariable(name = "path1") String path1, @PathVariable(name = "path2") String path2) {
+    public Object forward(HttpServletRequest request, HttpServletResponse response, @PathVariable(name = "path1") String path1, @PathVariable(name = "path2") String path2)throws GovernanceException {
         log.info("weevent url: /wevent/ {}  \"/\" {}", path1, path2);
         String forwarUrl = new StringBuffer(this.url).append("/").append(path1).append("/").append(path2).toString();
-        Object result = restTemplate.getForEntity(forwarUrl, Object.class).getBody();
-        return result;
+        try {
+            CloseableHttpResponse closeResponse = commonService.getCloseResponse(request, forwarUrl);
+            return EntityUtils.toString(closeResponse.getEntity());
+        } catch (Exception e) {
+            throw new GovernanceException(e.getMessage());
+        }
     }
 
-    @RequestMapping(value = "/weevent/admin/deploy_topic_control", method = RequestMethod.GET)
-    public Object forward() {
-        log.info("wevent url: /weevent/admin/deploy_topic_control");
-        String forwarUrl = new StringBuffer(this.url).append("/admin/deploy_topic_control").toString();
-        String result = restTemplate.getForEntity(forwarUrl, String.class).getBody();
-        return result;
-    }
 
-    @RequestMapping(value = "/weevent/{path1}/{path2}")
-    public Object forward(@PathVariable(name = "path1") String path1, @PathVariable(name = "path2") String path2,
-                          @RequestParam String topic) {
-        log.info("weevent url: /wevent/ {}  \"/\" {}", path1, path2);
-
-        String forwarUrl = new StringBuffer(this.url).append("/").append(path1).append("/").append(path2).append("?topic=").append(topic).toString();
-        Object result = restTemplate.getForEntity(forwarUrl, Object.class).getBody();
-        return result;
-    }
 }
---
+++ weevent-governance/src/test/java/com/webank/weevent/governance/junit/DAGDetectTest.java
@@ -0,0 +1,124 @@
+package com.webank.weevent.governance.junit;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import com.webank.weevent.governance.JUnitTestBase;
+import com.webank.weevent.governance.utils.DAGDetectUtil;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Slf4j
+public class DAGDetectTest extends JUnitTestBase {
+
+
+    @Autowired
+    private DAGDetectUtil dagDetectUtil;
+
+
+    @Before
+    public void before() {
+        log.info("=============================={}.{}==============================",
+                this.getClass().getSimpleName(),
+                this.testName.getMethodName());
+    }
+
+    @Test
+    public void testNormal001() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "C")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H", "I", "J")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void testNormal002() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "CEE", "DFF", "EAA", "FC")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H")));
+        map.put("C", new HashSet<>(Arrays.asList("D", "E", "F", "H", "I")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void testNormal003() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "C", "D", "E", "F")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H", "I", "J")));
+        map.put("C", new HashSet<>(Arrays.asList("D", "E", "F", "H", "I")));
+        map.put("D", new HashSet<>(Arrays.asList("E", "AA", "X", "W", "Y")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void testNormal004() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "C", "D", "E", "F")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H", "I", "J")));
+        map.put("C", new HashSet<>(Arrays.asList("D", "E", "F", "H", "I")));
+        map.put("D", new HashSet<>(Arrays.asList("E", "AA", "X", "W", "Y")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertTrue(b);
+    }
+
+    @Test
+    public void testException001() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "CEE", "DFF", "EAA", "FC")));
+        map.put("B", new HashSet<>(Arrays.asList("A", "E", "H", "I", "DJ")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertFalse(b);
+    }
+
+    @Test
+    public void testException002() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "CEE", "DFF", "EAA", "FC")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H", "I", "DJ")));
+        map.put("C", new HashSet<>(Arrays.asList("D", "E", "B", "H", "I")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertFalse(b);
+    }
+
+    @Test
+    public void testException003() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "CEE", "DFF", "EAA", "FC")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H", "I", "DJ")));
+        map.put("C", new HashSet<>(Arrays.asList("D", "E", "F", "H", "I")));
+        map.put("D", new HashSet<>(Arrays.asList("E", "A", "C", "W", "Y")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertFalse(b);
+    }
+
+    @Test
+    public void testException004() {
+        Map<String, Set<String>> map = new HashMap<>();
+        map.put("A", new HashSet<>(Arrays.asList("B", "CEE", "DFF", "EAA", "FC")));
+        map.put("B", new HashSet<>(Arrays.asList("C", "E", "H", "I", "DJ")));
+        map.put("C", new HashSet<>(Arrays.asList("D", "E", "F", "H", "I")));
+        map.put("D", new HashSet<>(Arrays.asList("E", "Abc", "C", "W", "Y")));
+        Set<String> topicSet = map.keySet();
+        boolean b = dagDetectUtil.checkLoop(map, topicSet);
+        Assert.assertFalse(b);
+    }
+
+
+}
