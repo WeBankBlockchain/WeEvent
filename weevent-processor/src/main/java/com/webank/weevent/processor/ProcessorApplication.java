394920e6b98b2ceec336b435708b8111ffb48514
crisiticmei(梅芬) <crisiticmei@webank.com>
2019-12-06 15:10:30 +0800

    move the datasource bean to the processorApplication

--- weevent-processor/src/main/java/com/webank/weevent/processor/ProcessorApplication.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/ProcessorApplication.java
@@ -1,17 +1,22 @@
 package com.webank.weevent.processor;
 
+import javax.sql.DataSource;
+
 import com.webank.weevent.processor.cache.CEPRuleCache;
 import com.webank.weevent.processor.config.ProcessorConfig;
 import com.webank.weevent.processor.mq.CEPRuleMQ;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Slf4j
@@ -20,6 +25,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class ProcessorApplication {
     public static ProcessorConfig processorConfig;
     public static ApplicationContext applicationContext;
+    public static org.springframework.core.env.Environment environment;
+
+    public static DataSource ds;
+
     @Autowired
     public void setContext(ApplicationContext context) {
         applicationContext = context;
@@ -38,12 +47,37 @@ public class ProcessorApplication {
         processorConfig = config;
     }
 
+    @Autowired
+    public void setEnvironment(org.springframework.core.env.Environment env) {
+        environment = env;
+    }
+
     private static void exit() {
         if (applicationContext != null) {
             System.exit(SpringApplication.exit(applicationContext));
         } else {
             System.exit(1);
         }
+    }
+
+    // private Enviroment env;
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public static DataSource getDs() {
+        try {
+
+            ds = new BasicDataSource();
+            ((BasicDataSource) ds).setDriverClassName(environment.getProperty("spring.datasource.driverClassName"));
+            ((BasicDataSource) ds).setUrl(environment.getProperty("spring.datasource.url"));
+            ((BasicDataSource) ds).setPassword(environment.getProperty("spring.datasource.password"));
+            ((BasicDataSource) ds).setUsername(environment.getProperty("spring.datasource.username"));
+
+            log.info("create ds:{}", ds);
+        } catch (Exception e) {
+            log.info("exception:", e.toString());
+        }
+        return ds;
     }
 
 
