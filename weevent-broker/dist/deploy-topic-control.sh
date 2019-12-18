7bbdb8b2f6602f174e3d32f24990bcf5c0e3050c
matthewliu <saintping@gmail.com>
2019-12-18 17:57:53 +0800

    remove check_java_jdk in bash (#409)

--- weevent-broker/dist/deploy-topic-control.sh
+++ weevent-broker/dist/deploy-topic-control.sh
@@ -7,23 +7,6 @@ if [[ -z ${JAVA_HOME} ]];then
    exit 1
 fi
 
-#check java jdk, not support openjdk 1.8 in CentOS
-function check_java_jdk(){
-    ${JAVA_HOME}/bin/java -version >>/dev/null 2>&1
-    if [[ $? -ne 0 ]];then
-        echo "not installed JDK"
-        exit 1
-    fi
-    java_version=$(${JAVA_HOME}/bin/java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $3 }' | awk -F[.] '{print $1$2}')
-    system_version=$(grep "=" /etc/os-release | head -1 | awk -F'[= "]' '{print $3}')
-    openjdk=$(${JAVA_HOME}/bin/java -version 2>&1 |awk 'NR==1{ gsub(/"/,""); print $1 }')
-    if [[ ${java_version} -le 18 && "${system_version}" == "CentOS" && "${openjdk}" == "openjdk" ]];then
-        echo "in CentOS, OpenJDK's version must be 1.9 or greater"
-        exit 1
-    fi
-}
-check_java_jdk
-
 ${JAVA_HOME}/bin/java -Xbootclasspath/a:./conf -cp ./apps/* -Dloader.path=./lib,../lib -Dloader.main=com.webank.weevent.broker.fisco.util.Web3sdkUtils org.springframework.boot.loader.PropertiesLauncher
 if [[ $? -ne 0 ]];then
     echo "deploy topic control failed."
