8fe70fa2081e72e0dbdc9975a3c2fbff124dbbb0
puremilkfan <811854137@qq.com>
2019-12-18 18:48:30 +0800

    issue #268: added support for spring boot built-in database h2 (#401)

--- weevent-governance/src/main/java/com/webank/weevent/governance/entity/base/PermissionBase.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/entity/base/PermissionBase.java
@@ -1,19 +1,25 @@
 package com.webank.weevent.governance.entity.base;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 /**
- *  PermissionBase class
+ * PermissionBase class
  *
  * @since 2019/10/15
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
+@MappedSuperclass
 public class PermissionBase extends BaseEntity {
 
+    @Column(name = "user_id")
     private Integer userId;
 
+    @Column(name = "broker_id")
     private Integer brokerId;
 
 }
