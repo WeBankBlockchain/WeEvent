2f0b5c7be6183cbc421838bc69219118f2521b64
puremilkfan <811854137@qq.com>
2019-12-18 18:48:30 +0800

    issue #268: added support for spring boot built-in database h2 (#401)

--- weevent-governance/src/main/java/com/webank/weevent/governance/entity/base/TopicBase.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/entity/base/TopicBase.java
@@ -1,5 +1,8 @@
 package com.webank.weevent.governance.entity.base;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,18 +13,26 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper=false)
+@MappedSuperclass
 public class TopicBase extends BaseEntity {
 
-    private String deleteAt;
-
+    @Column(name = "broker_id", columnDefinition = "int(11)")
     private Integer brokerId;
 
+    @Column(name = "group_id", columnDefinition = "varchar(64)")
     private String groupId;
 
+    @Column(name = "topic_name", columnDefinition = "varchar(128)")
     private String topicName;
 
+    @Column(name = "creater")
     private String creater;
 
+    @Column(name = "description")
     private String description;
+
+    //0 means not deleted ,others means deleted
+    @Column(name = "delete_at",nullable = false, columnDefinition = "BIGINT(16)")
+    private Long deleteAt = 0L;
 
 }
---
+++ weevent-governance/src/main/java/com/webank/weevent/governance/repository/RuleEngineRepository.java
@@ -0,0 +1,30 @@
+package com.webank.weevent.governance.repository;
+
+import java.util.List;
+
+import com.webank.weevent.governance.entity.RuleEngineEntity;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface RuleEngineRepository extends JpaRepository<RuleEngineEntity, Long> {
+
+    RuleEngineEntity findById(Integer id);
+
+
+    @Transactional
+    @Modifying
+    @Query(value = "update t_rule_engine  set status=2 , delete_at=:deleteAt where id =:id", nativeQuery = true)
+    void deleteRuleEngine(@Param("id") Integer id, @Param("deleteAt") Long deleteAt);
+
+
+    @Query(value = "select distinct from_destination as fromDestination,to_destination as toDestination from t_rule_engine" +
+            " where broker_id=:brokerId and group_id=:groupId and status != 2 and condition_type=1 and delete_at=0 and system_tag=0", nativeQuery = true)
+    List<RuleEngineEntity> getRuleTopicList(@Param("brokerId") Integer brokerId, @Param("groupId") String groupId);
+
+}
