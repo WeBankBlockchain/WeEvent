15608c35af53f4d0550c2cde4173f4ad75b159d3
puremilkfan <811854137@qq.com>
2019-12-25 09:18:38 +0800

    issue #388: replace fastjson to jackson (#412)

--- weevent-processor/src/main/java/com/webank/weevent/processor/ProcessorApplication.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/ProcessorApplication.java
@@ -1,15 +1,14 @@
 package com.webank.weevent.processor;
 
 
+import com.webank.weevent.governance.utils.H2ServerUtil;
 import com.webank.weevent.processor.cache.CEPRuleCache;
 import com.webank.weevent.processor.config.ProcessorConfig;
 import com.webank.weevent.processor.mq.CEPRuleMQ;
 
 import lombok.extern.slf4j.Slf4j;
-import org.h2.tools.Server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
@@ -26,30 +25,18 @@ public class ProcessorApplication {
     public static ApplicationContext applicationContext;
     public static Environment environment;
 
-    @Value("${spring.jpa.database:h2}")
-    private static String databaseType;
-
     @Autowired
     public void setContext(ApplicationContext context) {
         applicationContext = context;
     }
 
     public static void main(String[] args) throws Exception {
-        startH2();
+        H2ServerUtil.startH2();
         log.info("start processor success");
         SpringApplication app = new SpringApplication(ProcessorApplication.class);
         app.addListeners(new ApplicationPidFileWriter());
         app.run(args);
         log.info("start processor success");
-    }
-
-    private static void startH2() throws Exception {
-        if (!"h2".equals(databaseType.toLowerCase())) {
-            return;
-        }
-        Server server = Server.createTcpServer(new String[]{"-tcp", "-tcpAllowOthers", "-tcpPort", "7083"}).start();
-        String status = server.getStatus();
-        log.info("h2 status:{}", status);
     }
 
     @Autowired
