7ef96cc1ff984889e8266e007a1aca7b135bfebb
v_wbyqli <811854137@qq.com>
2019-12-26 14:29:57 +0800

    remove main method

--- weevent-governance/src/main/java/com/webank/weevent/governance/utils/JsonUtil.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/utils/JsonUtil.java
@@ -1,8 +1,6 @@
 package com.webank.weevent.governance.utils;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -68,44 +66,12 @@ public class JsonUtil {
             if (jsonString == null) {
                 return null;
             }
-            return objectMapper.readValue(jsonString, new TypeReference<List<RuleEngineConditionEntity>>() {});
+            return objectMapper.readValue(jsonString, new TypeReference<List<RuleEngineConditionEntity>>() {
+            });
         } catch (JsonProcessingException e) {
             log.error("convert jsonString to List failed ", e);
             throw new IOException("conversion of List failed", e);
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        Map<String, String> map = new HashMap<>();
-        RuleEngineConditionEntity ruleEngineConditionEntity = new RuleEngineConditionEntity();
-        RuleEngineConditionEntity ruleEngineConditionEntity1 = new RuleEngineConditionEntity();
-        RuleEngineConditionEntity ruleEngineConditionEntity2 = new RuleEngineConditionEntity();
-        ruleEngineConditionEntity.setColumnMark("1424");
-        ruleEngineConditionEntity.setColumnName("test");
-        List<RuleEngineConditionEntity> list = Arrays.asList(ruleEngineConditionEntity);
-
-
-        ruleEngineConditionEntity1.setColumnMark("1424");
-        ruleEngineConditionEntity1.setColumnName("test1");
-        List<RuleEngineConditionEntity> list1 = Arrays.asList(ruleEngineConditionEntity1);
-        ruleEngineConditionEntity.setChildren(list1);
-        ruleEngineConditionEntity2.setColumnMark("1424");
-        ruleEngineConditionEntity2.setColumnName("test2");
-        List<RuleEngineConditionEntity> list2 = Arrays.asList(ruleEngineConditionEntity2);
-        ruleEngineConditionEntity1.setChildren(list2);
-        map.put("columnName", ruleEngineConditionEntity.getColumnName());
-        map.put("children", JsonUtil.toJSONString(list1));
-        System.out.println(map.toString());
-        String mapString = JsonUtil.toJSONString(map);
-        Map<String, String> map1 = JsonUtil.parseObjectToMap(mapString, String.class, String.class);
-        System.out.println(map1);
-        List<RuleEngineConditionEntity> children = JsonUtil.objectMapper.readValue(map1.get("children"), new TypeReference<List<RuleEngineConditionEntity>>() {
-        });
-        assert children != null;
-        ruleEngineConditionEntity.setChildren(children);
-
-        String jsonString = JsonUtil.toJSONString(ruleEngineConditionEntity);
-        RuleEngineConditionEntity ruleEngineConditionEntity3 = JsonUtil.parseObject(jsonString, RuleEngineConditionEntity.class);
-        System.out.println(ruleEngineConditionEntity3.toString());
-    }
 }
