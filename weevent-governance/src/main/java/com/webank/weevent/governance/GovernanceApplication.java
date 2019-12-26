d3719c9fdfa2f80046449269b866a5b78ca51c91
v_wbyqli <811854137@qq.com>
2019-12-26 16:56:26 +0800

    fixed CI check issues

--- weevent-governance/src/main/java/com/webank/weevent/governance/GovernanceApplication.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/GovernanceApplication.java
@@ -418,69 +418,70 @@ public class GovernanceApplication {
             }
 
 
-            /**
-             *  We need to invoke sslSocket.setEnabledProtocols(new String[] {"SSLv3"});
-             */
-            class MyCustomSSLSocketFactory extends SSLSocketFactory {
-                private final SSLSocketFactory delegate;
-
-                public MyCustomSSLSocketFactory(SSLSocketFactory delegate) {
-                    this.delegate = delegate;
-                }
-
-                @Override
-                public String[] getDefaultCipherSuites() {
-                    return delegate.getDefaultCipherSuites();
-                }
-
-                @Override
-                public String[] getSupportedCipherSuites() {
-                    return delegate.getSupportedCipherSuites();
-                }
-
-                @Override
-                public Socket createSocket(final Socket socket, final String host, final int port, final boolean autoClose)
-                        throws IOException {
-                    final Socket underlyingSocket = delegate.createSocket(socket, host, port, autoClose);
-                    return overrideProtocol(underlyingSocket);
-                }
-
-                @Override
-                public Socket createSocket(final String host, final int port) throws IOException {
-                    final Socket underlyingSocket = delegate.createSocket(host, port);
-                    return overrideProtocol(underlyingSocket);
-                }
-
-                @Override
-                public Socket createSocket(final String host, final int port, final InetAddress localAddress,
-                                           final int localPort) throws IOException {
-                    final Socket underlyingSocket = delegate.createSocket(host, port, localAddress, localPort);
-                    return overrideProtocol(underlyingSocket);
-                }
-
-                @Override
-                public Socket createSocket(final InetAddress host, final int port) throws IOException {
-                    final Socket underlyingSocket = delegate.createSocket(host, port);
-                    return overrideProtocol(underlyingSocket);
-                }
-
-                @Override
-                public Socket createSocket(final InetAddress host, final int port, final InetAddress localAddress,
-                                           final int localPort) throws IOException {
-                    final Socket underlyingSocket = delegate.createSocket(host, port, localAddress, localPort);
-                    return overrideProtocol(underlyingSocket);
-                }
-
-                private Socket overrideProtocol(final Socket socket) {
-                    if (!(socket instanceof SSLSocket)) {
-                        throw new RuntimeException("An instance of SSLSocket is expected");
-                    }
-                    ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1"});
-                    return socket;
-                }
-            }
         };
         return simpleClientHttpRequestFactory;
+    }
+
+    /**
+     *  We need to invoke sslSocket.setEnabledProtocols(new String[] {"SSLv3"});
+     */
+    class MyCustomSSLSocketFactory extends SSLSocketFactory {
+        private final SSLSocketFactory delegate;
+
+        public MyCustomSSLSocketFactory(SSLSocketFactory delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return delegate.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return delegate.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(final Socket socket, final String host, final int port, final boolean autoClose)
+                throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(socket, host, port, autoClose);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final String host, final int port) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final String host, final int port, final InetAddress localAddress,
+                                   final int localPort) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port, localAddress, localPort);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final InetAddress host, final int port) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        @Override
+        public Socket createSocket(final InetAddress host, final int port, final InetAddress localAddress,
+                                   final int localPort) throws IOException {
+            final Socket underlyingSocket = delegate.createSocket(host, port, localAddress, localPort);
+            return overrideProtocol(underlyingSocket);
+        }
+
+        private Socket overrideProtocol(final Socket socket) {
+            if (!(socket instanceof SSLSocket)) {
+                throw new RuntimeException("An instance of SSLSocket is expected");
+            }
+            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1"});
+            return socket;
+        }
     }
 }
 
