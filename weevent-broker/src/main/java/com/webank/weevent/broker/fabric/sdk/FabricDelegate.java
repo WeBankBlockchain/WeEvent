444060d546af275ec53ca6df89c6f5ffd188eac7
anyspa <35908094+anyspa@users.noreply.github.com>
2019-12-27 09:57:19 +0800

    issue #389: fix fabric code codacy detection problem (#417)

--- weevent-broker/src/main/java/com/webank/weevent/broker/fabric/sdk/FabricDelegate.java
+++ weevent-broker/src/main/java/com/webank/weevent/broker/fabric/sdk/FabricDelegate.java
@@ -63,7 +63,7 @@ public class FabricDelegate {
         Fabric fabric = new Fabric(config);
         fabric.init(config.getChannelName());
         fabricMap.put(config.getChannelName(), fabric);
-        channels = FabricSDKWrapper.listChannelName(config);
+        channels = fabric.listChannelName(config);
         initRedisService();
     }
 
@@ -80,8 +80,24 @@ public class FabricDelegate {
         return this.fabricMap.get(channelName).getBlockHeight();
     }
 
-    public List<WeEvent> loop(Long blockNum, String channelName) throws BrokerException {
-        return this.fabricMap.get(channelName).loop(blockNum);
+    public List<WeEvent> loop(Long blockNum, String channelName) {
+        List<WeEvent> events = new ArrayList<>();
+        if (blockNum <= 0) {
+            return events;
+        }
+
+        // try to get data from local cache and redis
+        String key = getRedisKey(blockNum, channelName);
+        events = getFromCache(key);
+        // redis data may be dirty
+        if (events != null) {
+            return events;
+        }
+        events = this.fabricMap.get(channelName).loop(blockNum);
+
+        //write events list to redis server
+        setCache(key, events);
+        return events;
     }
 
     public static String getChannelName() {
@@ -94,6 +110,40 @@ public class FabricDelegate {
 
     public FabricConfig getFabricConfig() {
         return this.fabricConfig;
+    }
+
+    private String getRedisKey(Long blockNum, String channelName) {
+        return channelName.concat(Long.toString(blockNum));
+    }
+
+    private List<WeEvent> getFromCache(String key) {
+        try {
+            if (blockCache != null && blockCache.containsKey(key)) {
+                return blockCache.get(key);
+            }
+            if (redisTemplate != null) {
+                return redisTemplate.opsForValue().get(key);
+            }
+        } catch (Exception e) {
+            log.error("Exception happened while read events from redis server", e);
+        }
+
+        return null;
+    }
+
+    private void setCache(String key, List<WeEvent> events) {
+        try {
+            if (events != null) {
+                if (blockCache != null) {
+                    blockCache.putIfAbsent(key, events);
+                }
+                if (redisTemplate != null) {
+                    redisTemplate.opsForValue().set(key, events);
+                }
+            }
+        } catch (Exception e) {
+            log.error("Exception happened while write events to redis server", e);
+        }
     }
 
 }
