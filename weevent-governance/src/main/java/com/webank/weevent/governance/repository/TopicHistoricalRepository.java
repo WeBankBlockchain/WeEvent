1222bb026793d1025284eaa437b0f1ddee6d2e32
puremilkfan <811854137@qq.com>
2019-12-27 16:55:47 +0800

    issue #385: edit operation optimization of broker service (#416)

--- weevent-governance/src/main/java/com/webank/weevent/governance/repository/TopicHistoricalRepository.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/repository/TopicHistoricalRepository.java
@@ -2,10 +2,20 @@ package com.webank.weevent.governance.repository;
 
 import com.webank.weevent.governance.entity.TopicHistoricalEntity;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-public interface TopicHistoricalRepository extends JpaRepository<TopicHistoricalEntity,Long> {
+public interface TopicHistoricalRepository extends JpaRepository<TopicHistoricalEntity, Long> {
+
+
+    @Transactional
+    @Modifying
+    @Query(value = "delete from t_topic_historical where brokerId =:brokerId", nativeQuery = true)
+    void deleteTopicHistoricalByBrokerId(@Param("brokerId") Integer brokerId);
 
 }
