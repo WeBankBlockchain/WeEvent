c82687cbf47860dd6fadeec31ca6119375852aa5
puremilkfan <811854137@qq.com>
2019-12-27 16:55:47 +0800

    issue #385: edit operation optimization of broker service (#416)

--- weevent-governance/src/main/java/com/webank/weevent/governance/repository/RuleDatabaseRepository.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/repository/RuleDatabaseRepository.java
@@ -1,14 +1,18 @@
 package com.webank.weevent.governance.repository;
 
 
+import java.util.List;
+
 import com.webank.weevent.governance.entity.RuleDatabaseEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RuleDatabaseRepository extends JpaRepository<RuleDatabaseEntity,Long> {
+public interface RuleDatabaseRepository extends JpaRepository<RuleDatabaseEntity, Long> {
 
     RuleDatabaseEntity findById(Integer id);
+
+    List<RuleDatabaseEntity> findAllByBrokerIdAndSystemTag(Integer brokerId, Boolean tag);
 
 }
