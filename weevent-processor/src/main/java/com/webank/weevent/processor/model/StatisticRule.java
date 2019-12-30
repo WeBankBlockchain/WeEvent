4490dea519c8bed4edabf903b8f6c063e67a1e87
Cristic <CristicMay@gmail.com>
2019-12-30 15:20:19 +0800

    issue #384: add runing status statistic interface and get the rule's status in real time (#414)

--- weevent-processor/src/main/java/com/webank/weevent/processor/model/StatisticRule.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/model/StatisticRule.java
@@ -4,24 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
 public class StatisticRule implements Serializable {
     private String id;
+    private String brokerId;
+    private String ruleName;
+    private int status;
     private Date startTime;
 
-    @Value("0")
-    private int hitTimes;
-    @Value("0")
-    private int notHitTimes;
-    @Value("0")
-    private int publishEventSuccess;
-    @Value("0")
-    private int publishEventFail;
-    @Value("0")
-    private int writeDBSuccess;
-    @Value("0")
-    private int writeDBFail;
-    private String lastFailReason;
+    private int hitTimes = 0;
+    private int notHitTimes = 0;
+    private int dataFlowSuccess = 0;
+    private int dataFlowFail = 0;
+    private int destinationType = 0;
+    private String lastFailReason = "";
 }
