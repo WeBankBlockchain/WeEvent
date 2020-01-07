2278850ee0a011dca09c11e02605070e51b46135
puremilkfan <LYQ799123aq!>
2020-01-07 22:48:35 +0800

    verifierToken

--- weevent-governance/src/main/java/com/webank/weevent/governance/filter/UserFilter.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/filter/UserFilter.java
@@ -1,21 +1,19 @@
 package com.webank.weevent.governance.filter;
 
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.webank.weevent.governance.entity.AccountEntity;
 import com.webank.weevent.governance.utils.JwtUtils;
-
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 public class UserFilter extends OncePerRequestFilter {
@@ -25,6 +23,7 @@ public class UserFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader(JwtUtils.AUTHORIZATION_HEADER_PREFIX);
         if (!StringUtils.isBlank(token)) {
+            JwtUtils.verifierToken(token)
             AccountEntity accountEntity = JwtUtils.decodeToken(token, JwtUtils.PRIVATE_SECRET);
             if (accountEntity != null) {
                 log.info("get token from HTTP header, {} : {}", JwtUtils.AUTHORIZATION_HEADER_PREFIX, accountEntity.getUsername());
