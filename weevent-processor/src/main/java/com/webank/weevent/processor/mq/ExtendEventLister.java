7c285d006dd51634b5fc96f18f1dd208fce8da99
Cristic <CristicMay@gmail.com>
2020-01-16 18:02:29 +0800

    issue #365: optimize the subscription in processor (#430)

---
+++ weevent-processor/src/main/java/com/webank/weevent/processor/mq/ExtendEventLister.java
@@ -0,0 +1,52 @@
+package com.webank.weevent.processor.mq;
+
+import java.util.Map;
+
+import com.webank.weevent.processor.model.CEPRule;
+import com.webank.weevent.processor.model.StatisticWeEvent;
+import com.webank.weevent.processor.utils.JsonUtil;
+import com.webank.weevent.processor.utils.StatisticCEPRuleUtil;
+import com.webank.weevent.sdk.IWeEventClient;
+import com.webank.weevent.sdk.IWeEventClient.EventListener;
+import com.webank.weevent.sdk.WeEvent;
+
+import javafx.util.Pair;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ExtendEventLister implements EventListener {
+    private IWeEventClient client;
+    private Map<String, CEPRule> ruleMap;
+    private StatisticWeEvent statisticWeEvent = new StatisticWeEvent();
+
+    ExtendEventLister(IWeEventClient client, Map<String, CEPRule> ruleMap, StatisticWeEvent statisticWeEvent) {
+        this.client = client;
+        this.ruleMap = ruleMap;
+        this.statisticWeEvent = statisticWeEvent;
+    }
+
+    @Override
+    public void onEvent(WeEvent event) {
+        try {
+            String content = new String(event.getContent());
+            log.info("on event:{},content:{}", event.toString(), content);
+            Pair<String, String> type;
+            // check the content
+            if (JsonUtil.isValid(content)) {
+                type = CEPRuleMQ.handleOnEvent(client, event, ruleMap);
+            } else {
+                type = CEPRuleMQ.handleOnEventOtherPattern(client, event, ruleMap);
+            }
+            CEPRuleMQ.statisticWeEvent = StatisticCEPRuleUtil.statisticOrderType(statisticWeEvent, type);
+
+        } catch (Exception e) {
+            log.error(e.toString());
+        }
+    }
+
+    @Override
+    public void onException(Throwable e) {
+        log.info("on event:{}", e.toString());
+    }
+
+}
