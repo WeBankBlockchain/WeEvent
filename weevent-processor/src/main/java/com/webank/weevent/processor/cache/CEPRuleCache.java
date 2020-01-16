a887d4e2fa114ac5a250dabb3977047451c9bf0d
Cristic <CristicMay@gmail.com>
2020-01-16 18:02:29 +0800

    issue #365: optimize the subscription in processor (#430)

--- weevent-processor/src/main/java/com/webank/weevent/processor/cache/CEPRuleCache.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/cache/CEPRuleCache.java
@@ -4,16 +4,16 @@ import com.webank.weevent.processor.model.CEPRule;
 import com.webank.weevent.processor.mq.CEPRuleMQ;
 import com.webank.weevent.sdk.BrokerException;
 
+import javafx.util.Pair;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.*;
+import org.quartz.SchedulerException;
 
 
 @Slf4j
 public class CEPRuleCache {
 
-    public static void updateCEPRule(CEPRule rule, Map<String, CEPRule> ruleMap) throws BrokerException {
-        CEPRuleMQ.updateSubscribeMsg(rule, ruleMap);
+    public static void updateCEPRule(CEPRule rule, Pair<CEPRule, CEPRule> ruleBak) throws BrokerException, SchedulerException {
+        CEPRuleMQ.updateSubscribeMsg(rule, ruleBak);
     }
 
 }
