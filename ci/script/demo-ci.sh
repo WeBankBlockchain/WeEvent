3730501209db3deb2c675d5628cadfec156a2969
crisiticmei(梅芬) <crisiticmei@webank.com>
2020-01-20 11:44:49 +0800

    update the ci shell

--- ci/script/demo-ci.sh
+++ ci/script/demo-ci.sh
@@ -3,10 +3,20 @@
 echo "say hi"
 ls
 pwd
+brokerIp=${BROKER_INFO}
+nodes=${FISCOBCOS_NODE_INFO}
 
-## update processor the config
+echo ${brokerIp}
+echo ${nodes}
+
+echo "==============================================="
+  ## broker update the certificate
+  #rm -rf  ./weevent-broker/src/main/resources/v2/*
+  #mv ./ci/certificate/*  ./weevent-broker/src/main/resources/v2/
+
+  ## update processor the config
     application_properties=./weevent-processor/src/main/resources/application-dev.properties
-    brokerIp=127.0.0.1:7000
+    #brokerIp=127.0.0.1:7000
 
     if [[ -z ${brokerIp} ]];then
         echo "brokerIp is empty."
@@ -15,7 +25,47 @@ pwd
     else
        sed -i "/ci.broker.ip*/cci.broker.ip=${brokerIp}" ${application_properties}
     fi
-    echo "set server_port success"
-	  cat ./weevent-processor/src/main/resources/application-dev.properties
+    echo "set brokerIp success"
 
+	cat ./weevent-processor/src/main/resources/application-dev.properties
+
+
+  ## update the governance
+#	application_properties=./weevent-governance/src/main/resources/application-dev.properties
+#    if [[ -z ${brokerIp} ]];then
+#        echo "brokerIp is empty."
+#        echo "set brokerIp failed"
+#        exit 1
+#    else
+#       sed -i "/ci.broker.ip*/cci.broker.ip=${brokerIp}" ${application_properties}
+#    fi
+#    echo "set brokerIp success"
+#	cat ./weevent-governance/src/main/resources/application-dev.properties
+#
+#  ## update the broker ip
+#	application_properties=./weevent-broker/src/main/resources/application-dev.properties
+#    if [[ -z ${brokerIp} ]];then
+#        echo "brokerIp is empty."
+#        echo "set brokerIp failed"
+#        exit 1
+#    else
+#       sed -i "/ci.broker.ip*/cci.broker.ip=${brokerIp}" ${application_properties}
+#    fi
+#    echo "set brokerIp success"
+#
+#	cat ./weevent-governance/src/main/resources/application-dev.properties
+#
+#  ## update the broker fisco ip
+#  fisco_properties=./weevent-broker/src/main/resources/fisco.properties
+#
+#    if [[ -z ${nodes} ]];then
+#        echo "nodes is empty."
+#        echo "set nodes failed"
+#        exit 1
+#    else
+#       sed -i "/nodes*/cnodes=${nodes}" ${fisco_properties}
+#    fi
+#    echo "set nodes success"
+#
+#	cat ./weevent-broker/src/main/resources/fisco.properties
 echo "say hi2"
