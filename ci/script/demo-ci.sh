367bada40663c1d7dab0f471870a93f9f3c419ba
crisiticmei(梅芬) <crisiticmei@webank.com>
2020-01-20 10:54:27 +0800

    update the ci script

--- ci/script/demo-ci.sh
+++ ci/script/demo-ci.sh
@@ -2,6 +2,19 @@
 
 echo "say hi"
 ls
-
 pwd
+
+## update processor the config
+    application_properties=./weevent-processor/src/main/resources/application-dev.properties
+    brokerIp=127.0.0.1:7000
+
+    if [[ -z ${brokerIp} ]];then
+        echo "brokerIp is empty."
+        echo "set brokerIp failed"
+        exit 1
+    else
+       sed -i "/ci.broker.ip*/cci.broker.ip=${brokerIp}" ${application_properties}
+    fi
+    echo "set server_port success"
+
 echo "say hi2"
