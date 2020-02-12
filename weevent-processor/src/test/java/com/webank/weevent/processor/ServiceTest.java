913be809a1d4805e0cf7625ca1bad5821f34b49b
Cristic <CristicMay@gmail.com>
2020-02-12 21:37:32 +0800

    fix bug: fix statistics interface bug and optimize rule function 'now' (#446)

--- weevent-processor/src/test/java/com/webank/weevent/processor/ServiceTest.java
+++ weevent-processor/src/test/java/com/webank/weevent/processor/ServiceTest.java
@@ -37,6 +37,7 @@ public class ServiceTest {
 
     @Before
     public void setUp() {
+        String brokerUrl = wac.getEnvironment().getProperty("ci.broker.ip");
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
         rule.setId("1111");
         rule.setRuleName("test");
@@ -47,18 +48,16 @@ public class ServiceTest {
         rule.setConditionField("abs(a)<21 or floor(c)>10");
         rule.setToDestination("to.com.webank.weevent");
         rule.setDatabaseUrl("jdbc:mysql://127.0.0.1:3306/fromIfttt?user=root&password=111111");
-        rule.setBrokerUrl("http://127.0.0.1:7000/weevent");
+        rule.setBrokerUrl("http://"+brokerUrl+"/weevent");
         rule.setCreatedTime(new Date());
         rule.setStatus(1);
         rule.setUserId("1");
         rule.setGroupId("1");
         rule.setSystemTag("0");
         rule.setTableName("fromIfttt");
-        rule.setSystemFunctionMessage(null);
+        rule.setFunctionArray(null);
         rule.setConditionType(1);
-
     }
-
 
     @Test
     public void checkConditionRight() throws Exception {
@@ -70,6 +69,16 @@ public class ServiceTest {
         assertEquals(200, result.getResponse().getStatus());
     }
 
+
+    @Test
+    public void checkConditionRight6() throws Exception {
+        String url = "/checkWhereCondition";
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url).contentType(MediaType.APPLICATION_JSON).param("payload", "{\"a\":1,\"b\":\"2018-06-30 20:00:00\",\"c\":10}").param("condition", "c<10");
+        MvcResult result = mockMvc.perform(requestBuilder).andDo(print()).andReturn();
+        log.info("result:{}", result.getResponse().getContentAsString());
+        assertEquals(200, result.getResponse().getStatus());
+    }
 
     @Test
     public void checkConditionRight2() throws Exception {
@@ -139,7 +148,7 @@ public class ServiceTest {
         String arr = "";
         rule.setSelectField("a,b,c");
         rule.setConditionField("c<20 or a==10");
-        rule.setSystemFunctionMessage(arr);
+        rule.setFunctionArray(arr);
         rule.setConditionType(2);
         RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
         MvcResult result = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
@@ -148,33 +157,29 @@ public class ServiceTest {
     }
 
     @Test
-    public void getRuleDetails() throws Exception {
-        String url = "/getCEPRuleById";
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url).contentType(MediaType.APPLICATION_JSON).param("id", "11041548");
-        MvcResult result = mockMvc.perform(requestBuilder).andDo(print()).andReturn();
-        log.info("result:{}", result.getResponse().getContentAsString());
-        assertEquals(200, result.getResponse().getStatus());
-    }
-
-    @Test
-    public void getNoNRuleDetails() throws Exception {
-        String url = "/getCEPRuleById";
-
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url).contentType(MediaType.APPLICATION_JSON).param("id", "111");
-        MvcResult result = mockMvc.perform(requestBuilder).andDo(print()).andReturn();
-        log.info("result:{}", result.getResponse().getContentAsString());
-        assertEquals(200, result.getResponse().getStatus());
-    }
-
-    @Test
     public void deleteRule() throws Exception {
         String url = "/deleteCEPRuleById";
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).param("id", "11041548");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).param("id", "1111");
         MvcResult result = mockMvc.perform(requestBuilder).andDo(print()).andReturn();
         log.info("result:{}", result.getResponse().getContentAsString());
         assertEquals(200, result.getResponse().getStatus());
+    }
+
+    @Test
+    public void absHitRule() throws Exception {
+        String arr = "[[\"0\", \"6\", \"floor\", \"c\"]]";
+        rule.setFromDestination("from.com.webank.weevent");
+        rule.setPayload("{\"a\":1,\"b\":\"test\",\"c\":10}");
+        rule.setSelectField("a,eventId,topicName,brokerId,groupId");
+        rule.setConditionField("abs(c)<10");
+        rule.setToDestination("to.com.webank.weevent");
+        rule.setFunctionArray(arr);
+        rule.setConditionType(2);
+        RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result3 = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
+        log.info("result3:{}", result3.getResponse().getStatus());
+        assertEquals(200, result3.getResponse().getStatus());
     }
 
     @Test
@@ -183,22 +188,19 @@ public class ServiceTest {
         rule.setPayload("{\"a\":\"12345678901234567\",\"b\":20,\"c\":10,\"d\":10}");
         rule.setSelectField("a");
         rule.setConditionField("(b>=11 and (abs(c)!=22)) and abs(d)<=33");
-        rule.setSystemFunctionMessage(arr);
+        rule.setFunctionArray(arr);
         rule.setConditionType(2);
         rule.setId("20200102");
-
         RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
         MvcResult result3 = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
         log.info("result3:{}", result3.getResponse().getContentAsString());
         assertEquals(200, result3.getResponse().getStatus());
-        Thread.sleep(100000);
 
         rule.setFromDestination("testFromDestination");
         RequestBuilder requestBuilder4 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
         MvcResult result4 = mockMvc.perform(requestBuilder4).andDo(print()).andReturn();
         log.info("result4:{}", result4.getResponse().getContentAsString());
         assertEquals(200, result4.getResponse().getStatus());
-        Thread.sleep(100000);
 
         String url1 = "/statistic";
         RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url1).contentType(MediaType.APPLICATION_JSON).param("idList", "1104154821111");
@@ -225,5 +227,113 @@ public class ServiceTest {
         MvcResult result2 = mockMvc.perform(requestBuilder2).andDo(print()).andReturn();
         log.info("result:{}", result2.getResponse().getContentAsString());
         assertEquals(200, result2.getResponse().getStatus());
+    }
+
+
+    @Test
+    public void multiHitRule() throws Exception {
+        String arr ="[[\"0\",\"8\", \"abs\",\"age\"]]";
+        rule.setFromDestination("from.com.webank.weevent");
+        rule.setPayload("{\"age\":1,\"name\":\"rocky\"}");
+        rule.setSelectField("age,name,eventId,topicName,brokerId,groupId");
+        rule.setConditionField("abs(age)>20 and name!=\"name\"");
+        rule.setToDestination("to.com.webank.weevent");
+        rule.setFunctionArray(arr);
+        rule.setConditionType(1);
+        rule.setId("202002101213");
+        RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result3 = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
+        log.info("result3:{}", result3.getResponse().getStatus());
+        assertEquals(200, result3.getResponse().getStatus());
+    }
+
+    @Test
+    public void multiHitRule2() throws Exception {
+        String url1 = "/statistic";
+        String arr ="[[\"0\",\"9\",\"ceil\",\"age\"]]";
+        rule.setFromDestination("from.com.webank.weevent");
+        rule.setPayload("{\"age\":1,\"name\":\"rocky\"}");
+        rule.setSelectField("age,name,eventId,topicName,brokerId,groupId");
+        rule.setConditionField("ceil(age)>=20 and (age<=40 and name!=\"mark\")");
+        rule.setToDestination("to.com.webank.weevent");
+        rule.setFunctionArray(arr);
+        rule.setConditionType(1);
+        rule.setId("202002101213");
+        RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result3 = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
+        log.info("result3:{}", result3.getResponse().getStatus());
+        assertEquals(200, result3.getResponse().getStatus());
+
+        RequestBuilder requestBuilder5 = MockMvcRequestBuilders.get(url1).contentType(MediaType.APPLICATION_JSON).param("idList", "202002101213");
+        MvcResult result5 = mockMvc.perform(requestBuilder5).andDo(print()).andReturn();
+        log.info("result:{}", result5);
+        assertEquals(200, result5.getResponse().getStatus());
+
+        rule.setFromDestination("from.com.webank.weevent");
+        rule.setPayload("{\"age\":1,\"name\":\"rocky\"}");
+        rule.setSelectField("age,name,eventId,topicName,brokerId,groupId");
+        rule.setConditionField("ceil(age)>=20 and (age<=40 and name!=\"mark\")");
+        rule.setToDestination("to.com.webank.weevent");
+        rule.setFunctionArray(arr);
+        rule.setConditionType(1);
+        rule.setId("202002101214");
+        RequestBuilder requestBuilder6 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result6 = mockMvc.perform(requestBuilder6).andDo(print()).andReturn();
+        log.info("result3:{}", result6.getResponse().getStatus());
+        assertEquals(200, result6.getResponse().getStatus());
+
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url1).contentType(MediaType.APPLICATION_JSON).param("idList", "202002101214");
+        MvcResult result = mockMvc.perform(requestBuilder).andDo(print()).andReturn();
+        log.info("result:{}", result);
+        assertEquals(200, result.getResponse().getStatus());
+
+        // rule.setId("202002101215");
+        rule.setFromDestination("testFromDestination");
+        RequestBuilder requestBuilder4 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result4 = mockMvc.perform(requestBuilder4).andDo(print()).andReturn();
+        log.info("result4:{}", result4.getResponse().getContentAsString());
+        assertEquals(200, result4.getResponse().getStatus());
+
+        RequestBuilder requestBuilder2 = MockMvcRequestBuilders.get(url1).contentType(MediaType.APPLICATION_JSON).param("idList", "202002101213","202002101214","202002101215");
+        MvcResult result2 = mockMvc.perform(requestBuilder2).andDo(print()).andReturn();
+        log.info("result:{}", result3);
+        assertEquals(200, result2.getResponse().getStatus());
+    }
+
+    @Test
+    public void multiHitRule3() throws Exception {
+        String arr ="[[\"0\",\"6\", \"abs\",\"a\"]]";
+        rule.setFromDestination("from.com.webank.weevent");
+        rule.setPayload("{\"a\":1,\"b\":\"rocky\"}");
+        rule.setSelectField("a,b,eventId,topicName,brokerId,groupId");
+        rule.setConditionField("abs(a)>20");
+        rule.setToDestination("to.com.webank.weevent");
+        rule.setFunctionArray(arr);
+        rule.setConditionType(1);
+        rule.setId("202002101216");
+        RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result3 = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
+        log.info("result3:{}", result3.getResponse().getStatus());
+        assertEquals(200, result3.getResponse().getStatus());
+    }
+
+
+    @Test
+    public void nowSystemToTopic() throws Exception {
+        String arr = "[[\"10\",\"13\",\"now\",\"datatime\"]]";
+        String url = "/startCEPRule";
+        rule.setId("110000");
+        rule.setFromDestination("from.com.webank.weevent");
+        rule.setPayload("{\"a\":1,\"b\":\"test\",\"datatime\":\"2018-06-30 20:00:00\"}");
+        rule.setSelectField("a,eventId,currentDate");
+        rule.setConditionField("datatime<=now");
+        rule.setToDestination("to.com.webank.weevent");
+        rule.setFunctionArray(arr);
+        rule.setConditionType(1);
+        RequestBuilder requestBuilder3 = MockMvcRequestBuilders.post(url).contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJSONString(rule));
+        MvcResult result = mockMvc.perform(requestBuilder3).andDo(print()).andReturn();
+        assertEquals(200, result.getResponse().getStatus());
+        log.info("result3:{}", result);
     }
 }
