59ea0edc435e850cce49eb2c4e4cada9f6a37717
Cristic <CristicMay@gmail.com>
2020-02-14 17:46:44 +0800

    fix bugs: fixing the number system function can not reslove the double (#448)

--- weevent-processor/src/main/java/com/webank/weevent/processor/utils/CommonUtil.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/utils/CommonUtil.java
@@ -3,6 +3,7 @@ package com.webank.weevent.processor.utils;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -416,6 +417,24 @@ public class CommonUtil {
         Pattern pattern = Pattern
                 .compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
         Matcher m = pattern.matcher(strDate);
-        return m.matches();
+        return m.matches() && (strDate.contains("-") && strDate.contains(":"));
+    }
+
+    public static boolean isSimpleDate(String strDate) {
+        Pattern pattern = Pattern
+                .compile("^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))(\\s(((0?[0-9])|([1-2][0-3]))\\:([0-5]?[0-9])((\\s)|(\\:([0-5]?[0-9])))))?$");
+        Matcher m = pattern.matcher(strDate);
+        return m.matches() && strDate.contains("-");
+    }
+
+    public static boolean isTime(String strDate) {
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        boolean dateFlag = true;
+        try {
+            format.parse(strDate);
+        } catch (ParseException e) {
+            dateFlag = false;
+        }
+        return dateFlag;
     }
 }
