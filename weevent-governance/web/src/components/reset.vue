878e7f046d79beb48e88a46fb5a058f07f369ce3
wwwanhai <44886875+wwwanhai@users.noreply.github.com>
2020-02-23 20:58:10 +0800

    fix bugs: V1.2.0 test round1 (#460)

--- weevent-governance/web/src/components/reset.vue
+++ weevent-governance/web/src/components/reset.vue
@@ -71,11 +71,12 @@ export default {
               })
               this.login(data)
             } else {
-              this.$message({
+              this.$store.commit(this.$message({
                 type: 'warning',
                 message: this.$t('common.operFail'),
-                duration: 5000
-              })
+                duration: 0,
+                showClose: true
+              }))
             }
           })
         } else {
@@ -89,11 +90,12 @@ export default {
           localStorage.setItem('user', res.data.data.username)
           this.$router.push('./index')
         } else {
-          this.$message({
+          this.$store.commit(this.$message({
             type: 'warning',
             message: this.$t('userSet.loginFail'),
-            duration: 5000
-          })
+            duration: 0,
+            showClose: true
+          }))
         }
       })
     }
