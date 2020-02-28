c67741903cfca42e2a0df3e1e2cad26c28fab7e1
matthewliu <saintping@gmail.com>
2020-02-28 15:56:57 +0800

    fix bug: fix bug in check file context status and write FileChunksMeta file in utf8  (#484)

--- weevent-gateway/src/main/java/com/webank/weevent/filter/LogGlobalFilter.java
+++ weevent-gateway/src/main/java/com/webank/weevent/filter/LogGlobalFilter.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -21,18 +22,21 @@ import reactor.core.publisher.Mono;
 public class LogGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("{} R: {} {} {}",
+                request.getRemoteAddress(),
+                request.getId(),
+                request.getMethodValue(),
+                request.getURI());
+
         long startTimestamp = Calendar.getInstance().getTimeInMillis();
 
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            ServerHttpRequest request = exchange.getRequest();
-            log.info("{} {}(ms) {} {}, {} {} {}",
-                    request.getRemoteAddress(),
-                    Calendar.getInstance().getTimeInMillis() - startTimestamp,
-                    request.getURI().getScheme(),
-                    exchange.getResponse().getStatusCode(),
-                    request.getMethodValue(),
-                    request.getPath(),
-                    request.getQueryParams());
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("R: {}, {} {}(ms)",
+                    request.getId(),
+                    response.getStatusCode(),
+                    Calendar.getInstance().getTimeInMillis() - startTimestamp);
         }));
     }
 
