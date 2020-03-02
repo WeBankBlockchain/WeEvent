e3ade6ae507246df20d6fab5861394d5973a433a
matthewliu <saintping@gmail.com>
2020-03-02 17:53:43 +0800

    issue #471: provide weevent-core.jar that DO NOT depend on any server. (#488)

---
+++ weevent-core/src/main/java/com/webank/weevent/core/fabric/FabricBroker4Producer.java
@@ -0,0 +1,48 @@
+package com.webank.weevent.core.fabric;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+
+import com.webank.weevent.core.IProducer;
+import com.webank.weevent.core.fabric.sdk.FabricDelegate;
+import com.webank.weevent.core.fisco.util.ParamCheckUtils;
+import com.webank.weevent.sdk.BrokerException;
+import com.webank.weevent.sdk.JsonHelper;
+import com.webank.weevent.sdk.SendResult;
+import com.webank.weevent.sdk.WeEvent;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author websterchen
+ * @version v1.1
+ * @since 2019/8/10
+ */
+@Slf4j
+public class FabricBroker4Producer extends FabricTopicAdmin implements IProducer {
+    public FabricBroker4Producer(FabricDelegate fabricDelegate) {
+        super(fabricDelegate);
+    }
+
+    @Override
+    public boolean startProducer() {
+        return true;
+    }
+
+    @Override
+    public boolean shutdownProducer() {
+        return true;
+    }
+
+    @Override
+    public CompletableFuture<SendResult> publish(WeEvent event, String channelName) throws BrokerException {
+        log.debug("publish input param WeEvent: {}", event);
+
+        ParamCheckUtils.validateEvent(event);
+        this.validateChannelName(channelName);
+        return fabricDelegate.publishEvent(event.getTopic(),
+                channelName,
+                new String(event.getContent(), StandardCharsets.UTF_8),
+                JsonHelper.object2Json(event.getExtensions()));
+    }
+}
