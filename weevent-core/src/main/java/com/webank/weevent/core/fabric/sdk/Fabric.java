1da9a033829dd9a3a64357f4f82949d2ea84045a
anyspa <35908094+anyspa@users.noreply.github.com>
2020-03-11 18:23:58 +0800

    fix bug: optimize invokeCGI and JsonHelper code (#504)

--- weevent-core/src/main/java/com/webank/weevent/core/fabric/sdk/Fabric.java
+++ weevent-core/src/main/java/com/webank/weevent/core/fabric/sdk/Fabric.java
@@ -10,6 +10,13 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import com.webank.weevent.client.BrokerException;
+import com.webank.weevent.client.ErrorCode;
+import com.webank.weevent.client.JsonHelper;
+import com.webank.weevent.client.SendResult;
+import com.webank.weevent.client.TopicInfo;
+import com.webank.weevent.client.TopicPage;
+import com.webank.weevent.client.WeEvent;
 import com.webank.weevent.core.config.FabricConfig;
 import com.webank.weevent.core.dto.GroupGeneral;
 import com.webank.weevent.core.dto.ListPage;
@@ -20,13 +27,6 @@ import com.webank.weevent.core.fabric.dto.TransactionInfo;
 import com.webank.weevent.core.fisco.constant.WeEventConstants;
 import com.webank.weevent.core.fisco.util.DataTypeUtils;
 import com.webank.weevent.core.fisco.util.ParamCheckUtils;
-import com.webank.weevent.client.BrokerException;
-import com.webank.weevent.client.ErrorCode;
-import com.webank.weevent.client.JsonHelper;
-import com.webank.weevent.client.SendResult;
-import com.webank.weevent.client.TopicInfo;
-import com.webank.weevent.client.TopicPage;
-import com.webank.weevent.client.WeEvent;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
