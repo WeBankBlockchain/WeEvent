7d61d1bcf7a1202bcf4672471a6f4f67ba6427da
matthewliu <saintping@gmail.com>
2020-03-18 13:23:03 +0800

    issue #508: support request rate limited in local memory and remote redis  (#531)

--- weevent-gateway/src/main/java/com/webank/weevent/gateway/GatewayApplication.java
+++ weevent-gateway/src/main/java/com/webank/weevent/gateway/GatewayApplication.java
@@ -2,7 +2,9 @@ package com.webank.weevent.gateway;
 
 
 import com.webank.weevent.gateway.filter.Fix302GlobalFilter;
+import com.webank.weevent.gateway.filter.LocalMemoryLimiter;
 import com.webank.weevent.gateway.filter.LogGlobalFilter;
+import com.webank.weevent.gateway.filter.UriKeyResolver;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RateLimiter;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -38,5 +42,15 @@ public class GatewayApplication {
     @Bean
     public static GlobalFilter getLogGlobalFilter() {
         return new LogGlobalFilter();
+    }
+
+    @Bean
+    public RateLimiter<?> getLocalMemoryLimiter() {
+        return new LocalMemoryLimiter();
+    }
+
+    @Bean(name = UriKeyResolver.BEAN_NAME)
+    public KeyResolver getUriKeyResolver() {
+        return new UriKeyResolver();
     }
 }
