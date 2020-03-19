8222abf730a6ceedd645d0c70bcf8952cc66175b
matthewliu <saintping@gmail.com>
2020-03-19 19:08:50 +0800

    fix bugs: support QoS=0 and code refactor in MQTT (#534)

---
+++ weevent-broker/src/main/java/com/webank/weevent/broker/protocol/mqtt/store/AuthService.java
@@ -0,0 +1,28 @@
+package com.webank.weevent.broker.protocol.mqtt.store;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author websterchen
+ * @version v1.0
+ * @since 2019/6/1
+ */
+@Slf4j
+public class AuthService {
+    private String authAccount;
+    private String authPassword;
+
+    public AuthService(String authAccount, String authPassword) {
+        this.authAccount = authAccount;
+        this.authPassword = authPassword;
+    }
+
+    public boolean verifyUserName(String userName, String password) {
+        if (StringUtils.isBlank(this.authAccount) || StringUtils.isBlank(this.authPassword)) {
+            return true;
+        }
+
+        return this.authAccount.equals(userName) && this.authPassword.equals(password);
+    }
+}
