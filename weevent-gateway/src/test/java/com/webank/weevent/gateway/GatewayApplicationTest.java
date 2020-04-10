11c4b9b03878e7c3f081cde1cbad2e8c7f831a29
matthewliu <saintping@gmail.com>
2020-04-10 18:31:55 +0800

    issue 554: add /weevent-gateway/naming/list in gateway  (#558)

--- weevent-gateway/src/test/java/com/webank/weevent/gateway/GatewayApplicationTest.java
+++ weevent-gateway/src/test/java/com/webank/weevent/gateway/GatewayApplicationTest.java
@@ -1,14 +1,22 @@
 package com.webank.weevent.gateway;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.webank.weevent.gateway.dto.NamingService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -35,13 +43,28 @@ public class GatewayApplicationTest extends JUnitTestBase {
     }
 
     @Test
+    public void testListServices() {
+        ResponseEntity<Map<String, List<NamingService>>> rsp = this.restTemplate.exchange(this.url + "/weevent-gateway/naming/list",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<Map<String, List<NamingService>>>() {
+                },
+                new HashMap<>());
+
+        Assert.assertEquals(200, rsp.getStatusCodeValue());
+        Assert.assertNotNull(rsp.getBody());
+        Assert.assertTrue(rsp.getBody().containsKey("weevent-gateway"));
+        Assert.assertFalse(rsp.getBody().get("weevent-gateway").isEmpty());
+    }
+
+    @Test
     public void testBrokerGetVersion() {
         ResponseEntity<String> rsp = this.restTemplate.exchange(this.url + "/weevent-broker/admin/getVersion",
                 HttpMethod.GET,
                 null,
                 String.class,
                 new HashMap<>());
-        log.info("status: " + rsp.getStatusCode() + " body: " + rsp.getBody());
+        log.info("http code status: {} body: {}", rsp.getStatusCode(), rsp.getBody());
 
         Assert.assertEquals(200, rsp.getStatusCodeValue());
         Assert.assertNotNull(rsp.getBody());
@@ -49,13 +72,46 @@ public class GatewayApplicationTest extends JUnitTestBase {
     }
 
     @Test
-    public void testBrokerExistTopic() {
-        ResponseEntity<String> rsp = this.restTemplate.exchange(this.url + "/weevent-broker/rest/exist?topic=not_exist",
+    public void testBrokerFileHostNotExist() {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("file_host", "not_exist");
+            HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+            this.restTemplate.exchange(this.url + "/weevent-broker/admin/getVersion",
+                    HttpMethod.GET,
+                    requestEntity,
+                    String.class,
+                    new HashMap<>());
+
+            Assert.fail();
+        } catch (HttpServerErrorException.ServiceUnavailable e) {
+            //503 Service Unavailable
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testBrokerFileHost() {
+        ResponseEntity<Map<String, List<NamingService>>> services = this.restTemplate.exchange(this.url + "/weevent-gateway/naming/list",
                 HttpMethod.GET,
                 null,
+                new ParameterizedTypeReference<Map<String, List<NamingService>>>() {
+                },
+                new HashMap<>());
+
+        Assert.assertEquals(200, services.getStatusCodeValue());
+        Assert.assertNotNull(services.getBody());
+        Assert.assertTrue(services.getBody().containsKey("weevent-broker"));
+        Assert.assertFalse(services.getBody().get("weevent-broker").isEmpty());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("file_host", services.getBody().get("weevent-broker").get(0).getInstanceId());
+        HttpEntity<?> requestEntity = new HttpEntity<>(headers);
+        ResponseEntity<String> rsp = this.restTemplate.exchange(this.url + "/weevent-broker/admin/getVersion",
+                HttpMethod.GET,
+                requestEntity,
                 String.class,
                 new HashMap<>());
-        log.info("status: " + rsp.getStatusCode() + " body: " + rsp.getBody());
 
         Assert.assertEquals(200, rsp.getStatusCodeValue());
         Assert.assertNotNull(rsp.getBody());
