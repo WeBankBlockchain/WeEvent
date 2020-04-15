4d25824325c60e1cb73dc562f3011422ba6a6f94
chent57 <53847508+chent57@users.noreply.github.com>
2020-04-15 10:44:53 +0800

    issue #453: remove support for fisco-bcos version 1.3 (#556)

--- weevent-build/modules/broker/install-broker.sh
+++ weevent-build/modules/broker/install-broker.sh
@@ -38,23 +38,12 @@ copy_file
 
 sed -i "s/^.*nodes=.*$/nodes=${channel_info}/g" ${out_path}/conf/fisco.properties
 echo "set channel_info success"
-if [[ ${version} = "1.3" ]];then
-    sed -i "/version=2.0/cversion=1.3" ${out_path}/conf/fisco.properties
-    if [[ -f ${block_chain_node_path}/ca.crt ]] && [[ -f ${block_chain_node_path}/client.keystore ]]; then
-        rm -rf ${out_path}/conf/ca.crt ${out_path}/conf/client.keystore
-        cp ${block_chain_node_path}/ca.crt ${block_chain_node_path}/client.keystore ${out_path}/conf/
-    else
-        echo "ca.crt or client.keystore is not exist."
-        exit 1
-    fi
+if [[ -f ${block_chain_node_path}/ca.crt ]] && [[ -f ${block_chain_node_path}/node.crt ]] && [[ -f ${block_chain_node_path}/node.key ]]; then
+    rm -rf ${out_path}/conf/ca.crt ${out_path}/conf/node.crt ${out_path}/conf/node.key
+    cp ${block_chain_node_path}/ca.crt ${block_chain_node_path}/node.crt ${block_chain_node_path}/node.key ${out_path}/conf/
 else
-    if [[ -f ${block_chain_node_path}/ca.crt ]] && [[ -f ${block_chain_node_path}/node.crt ]] && [[ -f ${block_chain_node_path}/node.key ]]; then
-        rm -rf ${out_path}/conf/v2/ca.crt ${out_path}/conf/v2/node.crt ${out_path}/conf/v2/node.key
-        cp ${block_chain_node_path}/ca.crt ${block_chain_node_path}/node.crt ${block_chain_node_path}/node.key ${out_path}/conf/v2/
-    else
-        echo "ca.crt or node.crt or node.key is not exist."
-        exit 1
-    fi
+    echo "ca.crt or node.crt or node.key is not exist."
+    exit 1
 fi
 
 #deploy contract
