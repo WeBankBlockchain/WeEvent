2be718306f3da2594351af72837144468e6168ab
ck13243768917 <358489360@qq.com>
2020-05-06 12:59:27 +0800

    add local cobra rules

--- project/security_audit.sh
+++ project/security_audit.sh
@@ -58,12 +58,12 @@ scan_code()
 }
 
 install_cobra() {
-   pwd
-   ll
+
    git clone https://github.com/WhaleShark-Team/cobra.git
    echo -e "\033[32m 开始删除 \033[0m"
+   pwd
    rm  -rf cobra/rules/*
-   cp  project/rules  cobra/
+   cp  project/rules/*.xml  cobra/rules/
    cd  cobra/rules/
    ls
    echo -e "\033[32m 删除结束 \033[0m"
