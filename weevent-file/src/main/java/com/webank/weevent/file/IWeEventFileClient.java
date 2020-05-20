54b7ebd5eada303e627f59f9e192ca69f5a369b3
chent57 <53847508+chent57@users.noreply.github.com>
2020-05-20 14:42:55 +0800

    issue #548: separate large file transport into independent module (#588)

---
+++ weevent-file/src/main/java/com/webank/weevent/file/IWeEventFileClient.java
@@ -0,0 +1,115 @@
+package com.webank.weevent.file;
+
+import com.webank.weevent.client.BrokerException;
+import com.webank.weevent.client.SendResult;
+import com.webank.weevent.core.config.FiscoConfig;
+import com.webank.weevent.file.dto.FileTransportStats;
+import com.webank.weevent.file.service.FileChunksMeta;
+import com.webank.weevent.file.service.WeEventFileClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public interface IWeEventFileClient {
+    public static IWeEventFileClient build(String groupId, String filePath, int fileChunkSize, FiscoConfig fiscoConfig) {
+        return new WeEventFileClient(groupId, filePath, fileChunkSize, fiscoConfig);
+    }
+
+    /**
+     * @param topic topic name
+     */
+    void openTransport4Sender(String topic);
+
+    /**
+     * @param topic topic name
+     * @param publicPem public pem inputstream
+     * @throws BrokerException broker exception
+     */
+    void openTransport4Sender(String topic, InputStream publicPem) throws BrokerException;
+
+    /**
+     * @param topic topic name
+     * @param publicPem public pem path string
+     * @throws BrokerException broker exception
+     * @throws IOException IOException
+     */
+    void openTransport4Sender(String topic, String publicPem) throws BrokerException, IOException;
+
+
+    /**
+     * @param topic topic name
+     * @param fileListener notify interface
+     * @throws BrokerException broker exception
+     */
+    void openTransport4Receiver(String topic, FileListener fileListener) throws BrokerException;
+
+    /**
+     * @param topic topic name
+     * @param fileListener notify interface
+     * @param privatePem private key pem inputstream
+     * @throws BrokerException broker exception
+     */
+    void openTransport4Receiver(String topic, FileListener fileListener, InputStream privatePem) throws BrokerException;
+
+    /**
+     * @param topic topic name
+     * @param fileListener notify interface
+     * @param privatePem private key pem path string
+     * @throws IOException IOException
+     * @throws BrokerException InterruptedException
+     */
+    void openTransport4Receiver(String topic, FileListener fileListener, String privatePem) throws IOException, BrokerException;
+
+    /**
+     * invoke unSubTopic
+     *
+     * @param topic topic name
+     */
+    void closeTransport(String topic);
+
+    /**
+     * @param topic topic name
+     * @return filetransportstatus
+     */
+    FileTransportStats status(String topic);
+
+    /**
+     * @param topic topic name
+     * @return filechunksmeta list
+     */
+    List<FileChunksMeta> listFiles(String topic);
+
+    /**
+     * Publish a file to topic.
+     * The file's data DO NOT stored in block chain. Yes, it's not persist, may be deleted sometime after subscribe notify.
+     *
+     * @param topic binding topic
+     * @param localFile local file to be send
+     * @return send result, SendResult.SUCCESS if success, and return SendResult.eventId
+     * @throws BrokerException broker exception
+     * @throws IOException IOException
+     * @throws InterruptedException InterruptedException
+     */
+    SendResult publishFile(String topic, String localFile) throws BrokerException, IOException, InterruptedException;
+
+    /**
+     * Interface for file notify callback
+     */
+    interface FileListener {
+        /**
+         * Called while new file arrived.
+         *
+         * @param topicName topic name
+         * @param fileName  file name
+         */
+        void onFile(String topicName, String fileName);
+
+        /**
+         * Called while raise exception.
+         *
+         * @param e the e
+         */
+        void onException(Throwable e);
+    }
+}
