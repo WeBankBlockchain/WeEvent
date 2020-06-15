5b1e61f3d9853a3233859861250c4031e884ae1a
matthewliu <saintping@gmail.com>
2020-06-15 14:20:05 +0800

    merge conflict

--- weevent-core/src/main/java/com/webank/weevent/core/fisco/web3sdk/v2/SupportedVersion.java
+++ weevent-core/src/main/java/com/webank/weevent/core/fisco/web3sdk/v2/SupportedVersion.java
@@ -5,6 +5,7 @@ import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
