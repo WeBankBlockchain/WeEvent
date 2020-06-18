9ec647222f8738b7b565382fef21877def45ad55
wwwanhai <44886875+wwwanhai@users.noreply.github.com>
2020-06-18 16:05:11 +0800

    fix bug: fix codacy complaint and bugs on webpage (#613)

--- weevent-governance/web/src/components/tool/js/config.js
+++
@@ -1,8 +0,0 @@
-export const ACCEPT_CONFIG = {
-  image: ['.png', '.jpg', '.jpeg', '.gif', '.bmp'],
-  video: ['.mp4', '.rmvb', '.mkv', '.wmv', '.flv'],
-  document: ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.tif', '.tiff', '.zip'],
-  getAll () {
-    return [...this.image, ...this.video, ...this.document]
-  }
-}
