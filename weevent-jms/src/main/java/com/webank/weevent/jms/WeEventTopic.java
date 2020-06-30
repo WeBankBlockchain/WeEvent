0adedcf1ba0b9a8a5f674b423275fa5cf8448029
anyspa <anyspa@126.com>
2020-06-30 20:31:29 +0800

    optimize code

--- weevent-jms/src/main/java/com/webank/weevent/jms/WeEventTopic.java
+++ weevent-jms/src/main/java/com/webank/weevent/jms/WeEventTopic.java
@@ -5,59 +5,26 @@ import java.util.Map;
 
 import javax.jms.Topic;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * WeEvent JMS Topic.
  *
  * @author matthewliu
  * @since 2019/03/25
  */
+@Getter
+@Setter
 public class WeEventTopic implements Topic {
     private final String topicName;
 
     private String offset;
     private String groupId;
     private Map<String, String> extension;
-    private boolean isFile;
 
     public WeEventTopic(String topicName) {
         this.topicName = topicName;
-    }
-
-    @Override
-    public String getTopicName() {
-        return this.topicName;
-    }
-
-    public String getOffset() {
-        return this.offset;
-    }
-
-    public void setOffset(String offset) {
-        this.offset = offset;
-    }
-
-    public String getGroupId() {
-        return this.groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public Map<String, String> getExtension() {
-        return this.extension;
-    }
-
-    public void setExtension(Map<String, String> extension) {
-        this.extension = extension;
-    }
-
-    public boolean isFile() {
-        return this.isFile;
-    }
-
-    public void setFile(boolean file) {
-        this.isFile = file;
     }
 
 }
