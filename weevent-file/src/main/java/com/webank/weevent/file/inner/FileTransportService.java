1c897eb0c0bd503f56816cbad653c05253271f51
chent57 <53847508+chent57@users.noreply.github.com>
2020-10-26 10:25:59 +0800

    fix bug: reponse modify of bcos-java-sdk (#679)

--- weevent-file/src/main/java/com/webank/weevent/file/inner/FileTransportService.java
+++ weevent-file/src/main/java/com/webank/weevent/file/inner/FileTransportService.java
@@ -23,6 +23,7 @@ import com.webank.weevent.file.dto.FileTransportStats;
 import com.webank.weevent.file.service.FileChunksMeta;
 
 import lombok.extern.slf4j.Slf4j;
+import org.fisco.bcos.sdk.amop.AmopResponse;
 import org.fisco.bcos.sdk.model.AmopMsg;
 import org.fisco.bcos.sdk.model.Response;
 
@@ -236,14 +237,13 @@ public class FileTransportService {
         fileEvent.setChunkData(data);
 
         try {
-            Response rsp = channel.sendEvent(topic, fileEvent);
+            AmopResponse rsp = channel.sendEvent(topic, fileEvent);
             if (rsp.getErrorCode() != ErrorCode.SUCCESS.getCode()) {
                 log.error("receive sender chunk data to remote failed, rsp:{}", rsp.getErrorMessage());
                 throw AMOPChannel.toBrokerException(rsp);
             }
 
-            AmopMsg amopMsg = AMOPChannel.response2AmopMsg(rsp);
-            AmopMsgResponse amopMsgResponse = JsonHelper.json2Object(amopMsg.getData(), AmopMsgResponse.class);
+            AmopMsgResponse amopMsgResponse = JsonHelper.json2Object(rsp.getAmopMsgIn().getContent(), AmopMsgResponse.class);
             if (amopMsgResponse.getErrorCode() != ErrorCode.SUCCESS.getCode()) {
                 log.error("sender chunk data to remote failed, rsp:{}", amopMsgResponse.getErrorMessage());
                 throw AMOPChannel.toBrokerException(amopMsgResponse);
