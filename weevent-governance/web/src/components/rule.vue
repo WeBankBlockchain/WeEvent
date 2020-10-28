95cc6b48da84202b97c09572d70f237cc91b6b81
yangaobiao <312710721@qq.com>
2020-10-28 17:20:13 +0800

    bug fix：test bug modify (#681)

--- weevent-governance/web/src/components/rule.vue
+++ weevent-governance/web/src/components/rule.vue
@@ -232,7 +232,7 @@ export default {
       }
       API.ruleList(data).then(res => {
         if (res.data.code === 0) {
-          if (res.data.data) {
+          if (res.data.data && res.data.data.totalCount > 0) {
             this.ruleList = [].concat(res.data.data.ruleEngines)
             this.total = res.data.data.totalCount
           } else {
@@ -300,7 +300,7 @@ export default {
         } else {
           this.$store.commit('set_Msg', this.$message({
             type: 'warning',
-            message: res.data.data.message,
+            message: res.data.message,
             duration: 0,
             showClose: true
           }))
@@ -328,7 +328,7 @@ export default {
           } else {
             this.$store.commit('set_Msg', this.$message({
               type: 'warning',
-              message: res.data.data.message,
+              message: res.data.message,
               duration: 0,
               showClose: true
             }))
