3e5599771f0cb320f29d6ef175f62a955738eae6
yangaobiao <312710721@qq.com>
2020-11-03 11:27:42 +0800

    bug fix：delete import package

--- weevent-governance/src/main/java/com/webank/weevent/governance/repository/TransportStatusRepository.java
+++ weevent-governance/src/main/java/com/webank/weevent/governance/repository/TransportStatusRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.webank.weevent.governance.entity.FileTransportStatusEntity;
 
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
