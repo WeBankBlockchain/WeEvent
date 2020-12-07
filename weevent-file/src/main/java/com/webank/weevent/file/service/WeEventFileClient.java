d658a42a1ee3ee34310619a4243aa8716a2b1934
yangaobiao <312710721@qq.com>
2020-12-07 14:08:18 +0800

    bug fix:zipFiles modify (#701)

--- weevent-file/src/main/java/com/webank/weevent/file/service/WeEventFileClient.java
+++ weevent-file/src/main/java/com/webank/weevent/file/service/WeEventFileClient.java
@@ -378,38 +378,32 @@ public class WeEventFileClient implements IWeEventFileClient {
         }
         FileOutputStream fileOutputStream = null;
         ZipOutputStream zipOutputStream = null;
+        FileInputStream fileInputStream = null;
         try {
             fileOutputStream = new FileOutputStream(zipFile);
             zipOutputStream = new ZipOutputStream(fileOutputStream);
+            ZipEntry zipEntry = null;
             for (int i = 0; i < srcFiles.length; i++) {
-                FileInputStream fileInputStream = null;
-                ZipEntry zipEntry = null;
-                try {
-                    fileInputStream = new FileInputStream(srcFiles[i]);
-                    zipEntry = new ZipEntry(srcFiles[i].getName());
-                    zipOutputStream.putNextEntry(zipEntry);
-                    int len;
-                    byte[] buffer = new byte[1024];
-                    while ((len = fileInputStream.read(buffer)) > 0) {
-                        zipOutputStream.write(buffer, 0, len);
-                    }
-                } finally {
-                    if (null != fileInputStream) {
-                        fileInputStream.close();
-                    }
-                    if (zipOutputStream != null){
-                        zipOutputStream.closeEntry();
-                    }
+                fileInputStream = new FileInputStream(srcFiles[i]);
+                zipEntry = new ZipEntry(srcFiles[i].getName());
+                zipOutputStream.putNextEntry(zipEntry);
+                int len;
+                byte[] buffer = new byte[1024];
+                while ((len = fileInputStream.read(buffer)) > 0) {
+                    zipOutputStream.write(buffer, 0, len);
                 }
             }
         } finally {
-            if (fileOutputStream != null) {
+            if (null != zipOutputStream) {
+                zipOutputStream.closeEntry();
+                zipOutputStream.close();
+            }
+            if (null != fileInputStream) {
+                fileInputStream.close();
+            }
+            if (null != fileOutputStream) {
                 fileOutputStream.close();
             }
-            if (zipOutputStream != null) {
-                zipOutputStream.closeEntry();
-            }
-
         }
     }
 
