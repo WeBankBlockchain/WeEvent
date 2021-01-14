d7a8cabbbe367749dc363fb35059e04eb47a2927
yangaobiao <312710721@qq.com>
2021-01-14 10:29:33 +0800

    bug fix:add nacos

---
+++ weevent-core/src/main/java/com/webank/weevent/core/CoreApplication.java
@@ -0,0 +1,15 @@
+package com.webank.weevent.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entrance for broker application.
+ *
+ * @author matthewliu
+ * @since 2018/11/21
+ */
+@SpringBootApplication(scanBasePackages = {"com.webank.weevent.file", "com.webank.weevent.core.config"})
+public class CoreApplication {
+	
+
+}
--- weevent-core/src/main/java/com/webank/weevent/core/config/FiscoConfig.java
+++ weevent-core/src/main/java/com/webank/weevent/core/config/FiscoConfig.java
@@ -74,13 +74,4 @@ public class FiscoConfig {
     @Value("${bcosSDK.max_blocking_queue_size:102400}")
     private Integer maxBlockingQueueSize;
     
-    /**
-     * load configuration without spring
-     *
-     * @param configFile config file, if empty load from default location
-     * @return true if success, else false
-     */
-    public boolean load(String configFile) {
-        return new SmartLoadConfig().load(this, configFile, propertiesFileKey);
-    }
 }
