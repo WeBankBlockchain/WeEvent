5a0b07483e7dd3b31c082a136dc982b1b3bbeb5d
yangaobiao <312710721@qq.com>
2021-01-15 11:30:31 +0800

    bug fix:nacos modify

--- weevent-processor/src/main/java/com/webank/weevent/processor/config/ProcessorConfig.java
+++ weevent-processor/src/main/java/com/webank/weevent/processor/config/ProcessorConfig.java
@@ -1,6 +1,7 @@
 package com.webank.weevent.processor.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.ToString;
 @Data
 @ToString
 @Component
+@PropertySource(value = "classpath:processor.properties", encoding = "UTF-8")
 public class ProcessorConfig {
 
     @Value("${org.quartz.scheduler.instanceName:}")
